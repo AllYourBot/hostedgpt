dfad0dc72615fe304e766eee9b6a70632a02bc48
Keith Schacht <keith@explanation.com>
2023-12-17 13:04:41 -0800

    Rename app to HostedGPT

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "samantha_production"
+  # config.active_job.queue_name_prefix = "hostedgpt_production"
 
   config.action_mailer.perform_caching = false
 
