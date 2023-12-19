5210e245b12e7321257671d8fdf63cdb0d054cd2
Alireza Bashiri <al3rez@gmail.com>
2023-12-19 09:21:57 +0700

    refactor: add default timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_19_014006) do
 
   create_table "users", force: :cascade do |t|
     t.string "password"
+    t.datetime "registered_at", default: -> { "CURRENT_TIMESTAMP" }
   end
 
   add_foreign_key "tasks", "projects"
