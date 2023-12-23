06db020208c5d91cf5c5274642b96c8bee6aed1a
Alireza Bashiri <al3rez@gmail.com>
2023-12-23 11:34:26 +0700

    style: fmt

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :chat
-  belongs_to :parent, class_name: 'Message', optional: true
-  has_many :replies, class_name: 'Message', foreign_key: :parent_id, dependent: :destroy
+  belongs_to :parent, class_name: "Message", optional: true
+  has_many :replies, class_name: "Message", foreign_key: :parent_id, dependent: :destroy
 end
