6680983553724c076dbd62b4a9b53f4890d58bff
Alireza Bashiri <al3rez@gmail.com>
2023-12-24 18:03:07 +0700

    feat: stream messages

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,14 +1,28 @@
 class ChatsController < ApplicationController
-  def chat_messages_with_replies(chat_id)
-    Chat.find(chat_id).messages.where(parent_id: nil).includes(:replies)
-  end
-
   def show
-    @chats = Current.user.chats
-    @chat = @chats.find_by(id: params[:id]) || @chats.last
     # Fetch only those messages that are not replies
-    @messages = @chat.messages.where(parent_id: nil).includes(:replies)
+    @messages = current_chat.messages.where(parent_id: nil).includes(:replies)
     @has_answered = @messages.blank?
     render "home/show"
+  end
+
+  def create
+    @message = current_chat.messages.new(content: params[:message_content]) # Adjust this as per your message creation logic
+
+    if @message.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to chat_path(current_chat) }
+      end
+    else
+      redirect_to dashboard_path
+    end
+  end
+
+  private
+
+  def current_chat
+    @chats = Current.user.chats
+    @chat ||= @chats.find_by(id: params[:id]) || @chats.last
   end
 end
