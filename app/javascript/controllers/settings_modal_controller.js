b222e21de4b1aa0c400a128e4c186e4723712789
Alireza Bashiri <al3rez@gmail.com>
2023-12-31 15:19:18 +0700

    progress commit

--- app/javascript/controllers/settings_modal_controller.js
+++ app/javascript/controllers/settings_modal_controller.js
@@ -1,4 +1,5 @@
-import { Controller } from "@hotwired/stimulus"
+import Rails from "@rails/ujs";
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["modal", "openButton", "closeButton", "saveButton", "apiKeyEl", "firstNameEl", "lastNameEl"];
@@ -16,42 +17,12 @@ export default class extends Controller {
     this.modalTarget.style.display = "none";
   }
 
+  onPostSuccess() {
+    console.log("save")
+  }
+
   saveSettings() {
-    // Get input values
-    const apiKey = this.apiKeyElTarget.value;
-    const firstName = this.firstNameElTarget.value;
-    const lastName = this.lastNameElTarget.value;
-
-    // Prepare the data to send in the request body
-    const userData = {
-      user: {
-        openai_key: apiKey,
-        first_name: firstName,
-        last_name: lastName
-      }
-    };
-
-    // Send the update request using the fetch API
-    fetch(this.updateUrlValue, {
-      method: "PUT", // Use PUT method for update
-      headers: {
-        "Content-Type": "application/json", // Set the content type to JSON
-      },
-      body: JSON.stringify(userData), // Convert the data to JSON string
-    })
-      .then((response) => {
-        if (response.ok) {
-          // Update was successful, you can handle the success case here
-          console.log("Update successful");
-        } else {
-          // Update failed, handle the error case here
-          console.error("Update failed");
-        }
-      })
-      .catch((error) => {
-        // Handle any network or other errors here
-        console.error("Network error:", error);
-      });
+    Rails.fire(this.element.querySelector("form"), "submit")
 
     // Close the modal
     this.closeModal();
