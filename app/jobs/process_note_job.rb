c8a9dc660456e4c7a16cea02b43bb91d01a6d42b
Alireza Bashiri <al3rez@gmail.com>
2024-01-01 14:01:10 +0700

    progress commit

--- app/jobs/process_note_job.rb
+++ app/jobs/process_note_job.rb
@@ -1,21 +1,66 @@
 class ProcessNoteJob < ApplicationJob
+  RESPONSES_PER_MESSAGE = 1
+
   queue_as :default
 
   def perform(note_id, user_id)
-    user = User.find(user_id)
-    note = Note.find(note_id)
-    response = openai(user).chat(
-      parameters: {
-        model: "gpt-3.5-turbo", # Required.
-        messages: [{role: "user", content: note.content}], # Required.
-        temperature: 0.7
-      }
-    )
-    reply = note.replies.create!(content: response.dig("choices", 0, "message", "content"))
-    reply.broadcast_created
+    @user = User.find(user_id)
+    @note = Note.find(note_id)
+    OpenAI.configure do |config|
+      config.access_token = @user.openai_key
+    end
+    init_client
+    call_openai!
   end
 
-  def openai(user)
-    @client ||= OpenAI::Client.new(api_key: user.openai_key)
+  def call_openai!
+    messages = create_messages(note: @note)
+
+    @client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: for_openai(messages),
+        temperature: 0.8,
+        stream: stream_proc(messages),
+        n: RESPONSES_PER_MESSAGE
+      }
+    )
+  end
+
+  def for_openai(messages)
+    messages.map { |message| { role: "user", content: message.content } }
+  end
+
+  def create_messages(note:)
+    messages = []
+    RESPONSES_PER_MESSAGE.times do |i|
+      message = note.replies.create!(content: "")
+      message.broadcast_created
+      messages << message
+    end
+    messages
+  end
+
+  def stream_proc(messages)
+    proc do |chunk, _bytesize|
+      new_content = chunk.dig("choices", 0, "delta", "content")
+      finish_reason = chunk.dig("choices", 0, "finish_reason")
+      message = messages.first
+
+      if new_content.present?
+        # update the message in memory then broadcast
+        message.content += new_content
+        message.broadcast_updated
+      end
+
+      if finish_reason.present?
+        message.save!
+      end
+    end
+  end
+
+  def init_client
+    raise ArgumentError, "openai_key is missing" if @user.openai_key.blank?
+    @client ||= OpenAI::Client.new(api_key: @user.openai_key)
   end
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -4,8 +4,20 @@ class Reply < ApplicationRecord
   belongs_to :note, touch: true
   has_one :chat, through: :note
 
+  after_create_commit -> { broadcast_created }
+  after_update_commit -> { broadcast_updated }
+
   def broadcast_created
     broadcast_append_later_to(
+      "#{dom_id(chat)}_messages",
+      partial: "shared/reply",
+      locals: {message: self, scroll_to: true},
+      target: "#{dom_id(chat)}_messages"
+    )
+  end
+
+  def broadcast_updated
+    broadcast_append_to(
       "#{dom_id(chat)}_messages",
       partial: "shared/reply",
       locals: {message: self, scroll_to: true},
