0235583b437a5abae82e2510564021a523e04acc
Alireza Bashiri <al3rez@gmail.com>
2024-01-01 19:34:27 +0700

    update p content

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -4,6 +4,10 @@ class Reply < ApplicationRecord
   belongs_to :note, touch: true
   has_one :chat, through: :note
 
+  def self.for(message)
+    find_by(note: note)&.content || ""
+  end
+
   def broadcast_created
     broadcast_append_later_to(
       "#{dom_id(chat)}_messages",
@@ -15,10 +19,10 @@ class Reply < ApplicationRecord
 
   def broadcast_updated
     broadcast_append_to(
-      "#{dom_id(chat)}_messages",
-      partial: "shared/reply",
+      "#{dom_id(note)}_content",
+      partial: "shared/reply_content",
       locals: {message: self, scroll_to: true},
-      target: "#{dom_id(chat)}_messages"
+      target: "#{dom_id(note)}_content"
     )
   end
 end
