a8ab78b24fe3332369f694dee2bdcf57fa67148d
Alireza Bashiri <al3rez@gmail.com>
2024-01-06 20:49:45 +0700

    new refactoring is now working!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
-  config.active_job.queue_adapter = :good_job
+  config.active_job.queue_adapter = :solid_queue
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
