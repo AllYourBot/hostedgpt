f75082094b9469e1b7b34a2e47806c3c5426c47c
Rob <robacarp@users.noreply.github.com>
2024-01-11 15:52:25 -0700

    adds minimally functional page for assistant instructions (#45)

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,14 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+
+  fixtures :all
+
+  def login_as(user, password = "secret")
+    visit login_path
+    fill_in "email", with: user.person.email
+    fill_in "password", with: password
+    click_on "Continue"
+    assert_text "Successfully logged in."
+  end
 end
