f0f4d3dda39b4c25c77cc1e5d2f2f37586d81cbe
Keith Schacht <krschacht@gmail.com>
2024-01-11 17:30:35 -0600

    Migrate to new icons w/ helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+  def icon(name, opts = {})
+    opts = opts.symbolize_keys
+
+    variant = opts[:variant]&.to_sym || :solid
+    default_size = case variant
+      when :outline, :solid
+        24
+      when :mini
+        20
+      when :micro
+        16
+      end
+
+    size = opts[:size] || default_size
+    classes = opts[:class] || ""
+    raise "Do not include w-# or h-# in the class, use :size instead" if classes.match?(/(\bw-|\bh-)/)
+    classes += " w-[#{size}px] h-[#{size}px]"
+
+    if opts[:title]
+      direction = opts[:tooltip] || 'top'
+
+      content_tag(:div, class: classes + " tooltip tooltip-#{direction} hover:tooltip-open", data: { tip: opts[:title].to_s }) do
+        heroicon name, **opts
+      end
+    else
+      content_tag(:div, class: classes) do
+        heroicon name, **opts
+      end
+    end
+  end
 end
