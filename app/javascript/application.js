f164f649e9ff4471125891996b7a3a2358e6b05d
Keith Schacht <krschacht@gmail.com>
2024-01-14 17:31:43 -0600

    Replace Daisy with Flowbite

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 import Rails from "@rails/ujs";
-
+import 'flowbite';
 
 Rails.start();
