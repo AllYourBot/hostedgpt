9f0dbf94b3d3d1d35b05e262c04902244fadcb5c
Keith Schacht <krschacht@gmail.com>
2024-01-14 17:33:31 -0600

    Switch back to Daisy

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,5 @@
 import "@hotwired/turbo-rails";
 import "controllers";
 import Rails from "@rails/ujs";
-import 'flowbite';
 
 Rails.start();
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,3 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.2/app/assets/javascripts/rails-ujs.esm.js"
-pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.turbo.min.js"
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js" # flowbite added this, what is it?
