1657602f4d27610647b740a77a925b726b8cb9cf
Keith Schacht <krschacht@gmail.com>
2024-01-14 12:12:14 -0600

    Make textarea auto-expand

---
+++ app/javascript/controllers/textarea_autogrow_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+
+  initialize () {
+    this.onResize = undefined
+    this.resizeDebounceDelayValue = 100
+    this.autogrow = this.autogrow.bind(this)
+  }
+
+  connect () {
+    this.element.style.overflow = 'hidden'
+    this.onResize = this.resizeDebounceDelayValue > 0 ? debounce(this.autogrow, this.resizeDebounceDelayValue) : this.autogrow
+
+    this.autogrow()
+
+    this.element.addEventListener('input', this.autogrow)
+    window.addEventListener('resize', this.onResize)
+  }
+
+  disconnect () {
+    window.removeEventListener('resize', this.onResize)
+  }
+
+  autogrow () {
+    this.element.style.height = 'auto'
+    this.element.style.height = `${this.element.scrollHeight + 2}px`
+    console.log(`autogrow: ${this.element.style.height}`)
+  }
+}
+
+function debounce (callback, delay) {
+  return (...args) => {
+    const context = this
+    clearTimeout(timeout)
+
+    timeout = setTimeout(() => callback.apply(context, args), delay)
+  }
+}
