495467f9373fc67826022e800e08cfe44cc57a5b
Keith Schacht <krschacht@gmail.com>
2024-01-15 15:10:15 -0600

    Another click-toggle controller to hide sidebar

--- app/javascript/controllers/click_toggle_controller.js
+++ app/javascript/controllers/click_toggle_controller.js
@@ -2,20 +2,23 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static classes = [ "flippable" ]
-  static targets = [ "destination" ]
+  static targets = [ "trigger", "destination" ]
 
   connect () {
-    console.log('toggle connected', this.flippableClass)
-    this.element.addEventListener('mouseover', () => console.log('hovered'))
-    this.element.addEventListener('click', () => {
-      console.log(`clicked`, this.flippableClass)
-      this.destinationTargets.forEach(element => {
-        element.classList.toggle(this.flippableClass)
+    this.triggerTargets.forEach(triggerElement => {
+
+      triggerElement.addEventListener('click', () => {
+        console.log(`clicked`, this.flippableClass)
+        this.destinationTargets.forEach(element => {
+          element.classList.toggle(this.flippableClass)
+        })
       })
     })
   }
 
   disconnect () {
-    this.element.removeEventListener('click')
+    this.triggerTargets.forEach(triggerElement => {
+      triggerElement.removeEventListener('click')
+    })
   }
 }
