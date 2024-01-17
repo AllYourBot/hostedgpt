b278a9d3b9c30178ce1f578494f71f1632de7edd
rob <robert@explanation.com>
2024-01-17 16:34:20 -0700

    validates that a person email is unique

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ApplicationRecord
   delegated_type :personable, types: %w[User Tombstone]
   validates_associated :personable
+
+  validates :email, presence: true, uniqueness: true
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -4,4 +4,12 @@ class PersonTest < ActiveSupport::TestCase
   test "has an associated user" do
     assert_instance_of User, people(:keith_registered).user
   end
+
+  test "requires email addresses to be unique" do
+    person1 = users(:keith).person
+    person2 = Person.new(email: person1.email)
+    person2.save
+
+    assert person2.errors[:email].present?
+  end
 end
