c89491059dc53938358a43e06f5d1dbb5ea7701a
rob <robert@explanation.com>
2024-01-24 16:08:49 -0700

    formats and places toast messages

--- app/javascript/controllers/click_toggle_controller.js
+++ app/javascript/controllers/click_toggle_controller.js
@@ -3,7 +3,7 @@ import { Controller } from '@hotwired/stimulus'
 // This controller listens for a click on the "trigger" element. When it receives one, it toggles the "flippable" class on the "destination" element:
 //
 // The same element where you are defining the controller must get data-click-toggle-flippable-class="..class(es) named here..."
-// Any two elements at the same level as the controller or below get data-click-toggle-target="destination" and data-click-toggle-target="target"
+// Any two elements at the same level as the controller or below get data-click-toggle-target="destination" and data-click-toggle-target="trigger"
 
 export default class extends Controller {
   static classes = [ "flippable" ]
