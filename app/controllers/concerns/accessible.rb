f7e3c4a17330c48056a799503065485e4accc694
Keith Schacht <krschacht@gmail.com>
2024-01-28 17:01:34 -0500

    Simplify redirect

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -1,13 +1,8 @@
 module Accessible
   extend ActiveSupport::Concern
+
   included do
     skip_before_action :authenticate_user!, only: [:new, :create]
-    before_action :redirect_to_root, if: :user_signed_in?, only: [:new, :create]
-  end
-
-  protected
-
-  def redirect_to_root
-    redirect_to(root_path)
+    before_action -> { redirect_to(root_path) }, if: :user_signed_in?, only: [:new, :create]
   end
 end
