fb1d45f0d2904a6a799397285f747bfa82deab18
Keith Schacht <krschacht@gmail.com>
2024-02-01 15:51:38 -0600

    Update radio to be more robust

--- app/javascript/controllers/radio_behavior_controller.js
+++ app/javascript/controllers/radio_behavior_controller.js
@@ -9,6 +9,17 @@ import { Controller } from '@hotwired/stimulus'
 //
 // Each time one of the radio elements is clicked, it will get the "selected" class and all the others will have
 // the class removed.
+//
+// Note: It's possible that you want the data-action to be on an element which is inside the radio target. One
+// reason for this is that you want the selected class to be applied on an outer element wrapping a link. e.g.
+//
+//   <div data-radio-behavior-target="radio">
+//     <a href="#" data-action="radio-behavior#select">This wrapped link acts like a radio</a>
+//   </div>
+//
+// The controller handles this case. It detects that the click event does not have the target applied and finds
+// the closest element that has it. You cannot put the data-action on the outer div because stimulus intercepts
+// all clicks to a href's and does not bubble them up.
 
 
 export default class extends Controller {
@@ -20,6 +31,9 @@ export default class extends Controller {
       element.classList.remove(...this.selectedClasses)
     })
 
-    event.target.parentNode.classList.add(...this.selectedClasses)
+    if (this.radioTargets.includes(event.target))
+      event.target.classList.add(...this.selectedClasses)
+    else
+      event.target.closest('[data-radio-behavior-target="radio"]').classList.add(...this.selectedClasses)
   }
 }
