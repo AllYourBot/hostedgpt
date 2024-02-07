35b899adbabc073b81e4d2d621ba2fd6397cc59d
Keith Schacht <krschacht@gmail.com>
2024-02-07 16:50:35 -0600

    revise test

--- test/jobs/autotitle_conversation_job_test.rb
+++ test/jobs/autotitle_conversation_job_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class AutotitleConversationJobTest < ActiveJob::TestCase
-  test "sets conversation title to 'Hear me' when there are two messages" do
+  test "sets conversation title automatically when there are two messages" do
     conversation = conversations(:greeting)
 
     ChatCompletionAPI.stub :get_next_response, {"topic" => "Hear me"} do
@@ -11,7 +11,7 @@ class AutotitleConversationJobTest < ActiveJob::TestCase
     assert_equal "Hear me", conversation.reload.title
   end
 
-  test "sets conversation title even if there is only one message" do
+  test "sets conversation title automatically even if there is only one message" do
     conversation = conversations(:javascript)
     conversation.messages.sorted.last.destroy!
 
@@ -22,8 +22,9 @@ class AutotitleConversationJobTest < ActiveJob::TestCase
     assert_equal "Javascript popState", conversation.reload.title
   end
 
-  test "the topic is not set if the conversation " do
+  test "the topic is not set if the conversation has no messages" do
     conversation = users(:keith).conversations.create!(assistant: assistants(:samantha))
+    conversation.update!(updated_at: Time.current) # update is what triggers the callback
 
     assert_nothing_raised do # confirms the exception did not raise outside the job
       ChatCompletionAPI.stub :get_next_response, {"topic" => "This will not be set"} do
