323a20027408f85b378df10ac7e40d3b134fed1e
Keith Schacht <krschacht@gmail.com>
2024-02-07 17:43:09 -0600

    Style composer based on image support

--- app/services/conversation_ai.rb
+++ app/services/conversation_ai.rb
@@ -106,6 +106,7 @@ class ConversationAi
       "gpt-4-32k": 32768,
       "gpt-4-0613": 8192,
       "gpt-4-32k-0613": 32768,
+      'gpt-3.5-turbo-0125' => 16385,
       "gpt-3.5-turbo-1106": 16385,
       "gpt-3.5-turbo": 4096,
       "gpt-3.5-turbo-16k": 16385,
