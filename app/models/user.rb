5806a1d190f37843e3aa8b62fe23f5d068128ef4
Keith Schacht <krschacht@gmail.com>
2024-02-08 16:55:08 -0600

    Get tests passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
 
   validates :password, length: { minimum: 6 }, allow_nil: true
 
-  has_many :chats, dependent: :destroy
   has_many :assistants, dependent: :destroy
   has_many :conversations, dependent: :destroy
 end
