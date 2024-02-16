cac03d9a405b99abe7b328546880275413da0883
Kaleshe <kalesheav@gmail.com>
2024-02-16 11:50:56 +0000

    Copy message to clipboard

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["message"];
+
+  copy() {
+    navigator.clipboard.writeText(this.message);
+
+    this.element.querySelector(
+      "div[data-action='click->clipboard#copy']"
+    ).dataset.tip = "Copied!";
+  }
+
+  get message() {
+    return this.messageTarget.innerText;
+  }
+}
