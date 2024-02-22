720d77179e3dadbfc3070c200cec2566029ebf93
Florian Felsing <florian.felsing@googlemail.com>
2024-02-22 17:19:58 +0100

    Extend theme with ChatGPT grayscale

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,7 +13,20 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        'graphite': 'rgba(52,53,65,1)'
+        'graphite': 'rgba(52,53,65,1)',
+        'gray': {
+          50: '#f9f9f9',
+          100: '#ececec',
+          200: '#cdcdcd',
+          300: '#b4b4b4',
+          400: '#9b9b9b',
+          500: '#676767',
+          600: '#424242',
+          700: '#2f2f2f',
+          800: '#212121',
+          900: '#171717',
+          950: '#0d0d0d',
+        },
       },
       scale: {
         '96': '0.96',
