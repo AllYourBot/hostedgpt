27657f2d5606de62891be6c76ec39b8b49aec8b3
Kaleshe <kalesheav@gmail.com>
2024-02-28 19:48:38 +0000

    rename message ref to text

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,17 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["message"]
+  static targets = ["text"]
 
   copy() {
-    navigator.clipboard.writeText(this.message)
+    navigator.clipboard.writeText(this.text)
 
     this.element.querySelector(
       "div[data-action='click->clipboard#copy']"
     ).dataset.tip = "Copied!"
   }
 
-  get message() {
-    return this.messageTarget.innerText
+  get text() {
+    return this.textTarget.innerText
   }
 }
