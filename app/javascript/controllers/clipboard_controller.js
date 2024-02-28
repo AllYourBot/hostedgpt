317086f7d0d92647d25892eb788e591ae91d229d
Kaleshe <kalesheav@gmail.com>
2024-02-28 19:43:41 +0000

    removed semi colons from js file

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,19 +1,17 @@
-"use strict";
-
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["message"];
+  static targets = ["message"]
 
   copy() {
-    navigator.clipboard.writeText(this.message);
+    navigator.clipboard.writeText(this.message)
 
     this.element.querySelector(
       "div[data-action='click->clipboard#copy']"
-    ).dataset.tip = "Copied!";
+    ).dataset.tip = "Copied!"
   }
 
   get message() {
-    return this.messageTarget.innerText;
+    return this.messageTarget.innerText
   }
 }
