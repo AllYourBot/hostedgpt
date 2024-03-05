3774b87f7846876b40b45b05036ab13f3722fdd1
Keith Schacht <krschacht@gmail.com>
2024-03-05 10:17:37 -0600

    fix formatting

--- app/models/concerns/scopes/lib/boolean.rb
+++ app/models/concerns/scopes/lib/boolean.rb
@@ -1,16 +1,14 @@
 module Scopes::Lib::Boolean
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-    included do
-        boolean_columns = columns.select do |column|
-            column.type == :boolean
-        end
-
-        boolean_columns.each do |column|
-            auto_scope "#{column.name}", -> { where(column.name => true) },
-                column: column.name
-            auto_scope "not_#{column.name}", -> { where(column.name => false) },
-                column: column.name
-        end
+  included do
+    boolean_columns = columns.select do |column|
+      column.type == :boolean
     end
+
+    boolean_columns.each do |column|
+      auto_scope "#{column.name}", -> { where(column.name => true) }, column: column.name
+      auto_scope "not_#{column.name}", -> { where(column.name => false) }, column: column.name
+    end
+  end
 end
