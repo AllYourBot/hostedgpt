7d943297a69958d1068f7dd9f96c2ef5d64cb603
Keith Schacht <krschacht@gmail.com>
2024-03-05 08:08:04 -0600

    remove simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,38 +4,6 @@ require "rails/test_help"
 require "minitest/autorun"
 require "pry"
 
-IS_FULL_TEST_RUN = ARGV.none? { |arg| arg.include?("_test.rb") || arg.include?("test/") }
-
-if IS_FULL_TEST_RUN
-  require "simplecov"
-
-  SimpleCov.start "rails" do
-    add_filter do |source_file|
-      next unless source_file.lines.count > 0
-      source_file.lines.first.src =~ /# ignore simplecov/i ||
-        source_file.filename =~ /application_.*\.rb/
-    end
-  end
-
-  SimpleCov.at_exit do
-    SimpleCov.result.format!
-    zero_coverage_files = SimpleCov.result.files.select { |file| file.covered_percent == 0 }
-    if zero_coverage_files.any?
-      puts ""
-      puts "WARNING: #{zero_coverage_files.length} files with zero tests. Add this comment to the top of any file you never plan to add tests for: # ignore simplecov"
-      puts ""
-      root_path = Rails.root.to_s + "/"
-      zero_coverage_files.each do |file|
-        puts file.filename.sub(root_path, "")
-      end
-
-      puts ""
-      puts "WARNING: #{zero_coverage_files.length} files have no tests. ^ Can you test any of them?"
-      puts ""
-    end
-  end
-end
-
 class ActionDispatch::IntegrationTest
   include Rails.application.routes.url_helpers
 
