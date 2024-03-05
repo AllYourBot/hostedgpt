5d034b64759a8247cbf3eeeff953ca5a96971c3b
Keith Schacht <krschacht@gmail.com>
2024-03-05 16:09:51 -0600

    Move feature stimulus into a namespace

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,7 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/utils", under: "utils"
+
+Dir["app/javascript/**/*"].select { |d| File.directory?(d) }.each do |dir|
+  pin_all_from dir, under: dir.remove("app/javascript/")
+end
