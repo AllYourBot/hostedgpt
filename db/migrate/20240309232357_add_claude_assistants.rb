c0cb5868f36822d5756fae5fe17bc9a2ef8f1f23
Keith Schacht <krschacht@gmail.com>
2024-03-09 17:26:45 -0600

    Create Claude assistants

---
+++ db/migrate/20240309232357_add_claude_assistants.rb
@@ -0,0 +1,12 @@
+class AddClaudeAssistants < ActiveRecord::Migration[7.1]
+  def up
+    User.all.each do |user|
+      user.assistants.create! name: "Claude 3 Opus", model: "claude-3-opus-20240229", images: true
+      user.assistants.create! name: "Claude 3 Sonnet", model: "claude-3-sonnet-20240229", images: true
+    end
+  end
+
+  def down
+    Assistant.name_like('Claude').destroy_all
+  end
+end
