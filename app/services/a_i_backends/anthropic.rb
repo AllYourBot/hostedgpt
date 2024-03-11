a7f19c531089aef3af2ebbff7dd5529b57a1cf06
Keith Schacht <krschacht@gmail.com>
2024-03-11 14:19:19 -0600

    Add anthropic backend w/ tests

---
+++ app/services/a_i_backends/anthropic.rb
@@ -0,0 +1,84 @@
+class AIBackends::Anthropic
+  attr :client
+
+  # Rails system tests don't seem to allow mocking because the server and the
+  # test are in separate processes.
+  #
+  # In regular tests, mock this method or the TestClients::Anthropic class to do
+  # what you want instead.
+  def self.client
+    if Rails.env.test?
+      TestClients::Anthropic
+    else
+      Anthropic::Client
+    end
+  end
+
+  def initialize(user, assistant, conversation, message)
+    @client = self.class.client.new(access_token: user.anthropic_key)
+    @assistant = assistant
+    @conversation = conversation
+    @message = message
+  end
+
+  def get_next_chat_message(&chunk_received_handler)
+    response_handler = proc do |intermediate_response, bytesize|
+      chunk = intermediate_response.dig("delta", "text")
+      print chunk if Rails.env.development?
+      yield chunk if chunk
+    rescue ::GetNextAIMessageJob::ResponseCancelled => e
+      raise e
+    rescue => e
+      puts "\nError in AIBackends::OpenAI response handler: #{e.message}"
+      puts e.backtrace
+    end
+
+    response_handler = nil unless block_given?
+
+    response = @client.messages(
+      model: @assistant.model,
+      system: @assistant.instructions,
+      messages: existing_messages,
+      parameters: {
+        max_tokens: 2000, # we should really set this dynamically, based on the model, to the max
+        stream: response_handler,
+      }
+    )
+
+    if response.dig("content")
+      response.dig("content", 0, "text")
+    else
+      response
+    end
+  end
+
+  private
+
+  def existing_messages
+    @conversation.messages.ordered.where("created_at < ?", @message.created_at).collect do |message|
+      if @assistant.images && message.documents.present?
+
+        content = [{ type: "text", text: message.content_text }]
+        content += message.documents.collect do |document|
+          { type: "image",
+            source: {
+              type: "base64",
+              media_type: document.file.blob.content_type,
+              data: document.file_base64,
+            }
+          }
+        end
+
+        {
+          role: message.role,
+          content: content
+        }
+      else
+        {
+          role: message.role,
+          content: message.content_text
+        }
+      end
+    end
+  end
+end
---
+++ test/services/a_i_backends/anthropic_test.rb
@@ -0,0 +1,45 @@
+require "test_helper"
+
+module AIBackends
+  class AnthropicTest < ActiveSupport::TestCase
+    setup do
+      @conversation = conversations(:attachments)
+      @anthropic = Anthropic.new(users(:keith), assistants(:samantha), @conversation, @conversation.latest_message)
+      @test_client = TestClients::Anthropic.new(access_token: 'abc')
+    end
+
+    test "initializing client works" do
+      assert @anthropic.client.present?
+    end
+
+    test "get_next_chat_message works" do
+      assert_equal @test_client.messages.dig("content", 0, "text"), @anthropic.get_next_chat_message
+    end
+
+    test "existing_messages constructs a proper response and pivots on images" do
+      existing_messages = @anthropic.send(:existing_messages)
+
+      assert_equal @conversation.messages.length-1, existing_messages.length
+
+      @conversation.messages.ordered.each_with_index do |message, i|
+        next if @conversation.messages.length == i+1
+
+        if message.documents.present?
+          assert_instance_of Array, existing_messages[i][:content]
+          assert_equal message.documents.length+1, existing_messages[i][:content].length
+        else
+          assert_equal existing_messages[i][:content], message.content_text
+        end
+      end
+    end
+
+    test "existing_messages only considers messages up to the assistant message being generated" do
+      @anthropic = Anthropic.new(users(:keith), assistants(:samantha), @conversation, messages(:yes_i_can))
+
+      existing_messages = @anthropic.send(:existing_messages)
+
+      assert_equal 1, existing_messages.length
+      assert_equal existing_messages[0][:content], messages(:can_you_hear).content_text
+    end
+  end
+end
