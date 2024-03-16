e717d5566a78e8cd2ed3a53d2d8bd45981a73c54
Olivier <olimart@users.noreply.github.com>
2024-03-16 11:12:34 -0400

    Inflections (#198)

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -11,6 +11,8 @@
 # end
 
 # These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym "RESTful"
-# end
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  # inflect.acronym "RESTful"
+  inflect.acronym "AI"
+  inflect.acronym "API"
+end
