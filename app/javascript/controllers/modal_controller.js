bd115d3a7682b6d03ff2bd911c7d06a4d78094ea
Keith Schacht <krschacht@gmail.com>
2024-03-17 11:29:37 -0500

    Finish modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,7 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   open(event) {
-    if (event.key != '?' || !event.shiftKey) return
+    if (event.key != '?' || !event.shiftKey || ["INPUT", "TEXTAREA"].includes(event.target.tagName)) return
+
     this.element.showModal()
   }
 }
