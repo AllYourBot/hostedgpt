d4b830f991d24579589e2e46af439598139b73f5
Keith Schacht <krschacht@gmail.com>
2024-03-17 22:18:53 -0500

    fix jumpy after submit

--- app/javascript/controllers/textarea_autogrow_controller.js
+++ app/javascript/controllers/textarea_autogrow_controller.js
@@ -9,24 +9,24 @@ export default class extends Controller {
   }
 
   connect() {
-    console.log('connect')
     this.element.style.overflow = 'hidden'
     this.autogrow()
 
     this.element.addEventListener('input', this.throttledAutogrow)
+    window.addEventListener('turbo:morph', () => this.throttledAutogrow)
     window.addEventListener('resize', this.throttledAutogrow)
     window.addEventListener('main-column-changed', this.throttledAutogrow)
   }
 
   disconnect() {
     this.element.removeEventListener('input', this.throttledAutogrow)
+    window.removeEventListener('turbo:morph', this.throttledAutogrow)
     window.removeEventListener('resize', this.throttledAutogrow)
     window.removeEventListener('main-column-changed', this.throttledAutogrow)
   }
 
   throttledAutogrow = throttle(() => this.autogrow(), 50)
   autogrow() {
-    console.log('autogrow')
     const prevHeight = getComputedStyle(this.element).height
     this.element.style.height = 'auto'
     const newHeight = `${this.element.scrollHeight+2}px` // scrollHeight is two less than computedHeight, this prevents jumps
