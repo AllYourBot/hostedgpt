d75b1cb28a5ff63d290bd451fb9443e4a765fa5f
Keith Schacht <krschacht@gmail.com>
2024-03-24 20:52:04 -0500

    revert

--- app/javascript/controllers/keyboard_controller.js
+++ app/javascript/controllers/keyboard_controller.js
@@ -1,16 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "keyboardable" ]
-
   click() {
-    let element
+    if (this.elementVisible()) this.element.click()
+  }
 
-    if (this.hasKeyboardableTarget)
-      element = this.keyboardableTargets.slice(-1)[0]
-    else
-      element = this.element
-
-    element.click()
+  elementVisible() {
+    return (this.element.offsetParent != null)
   }
 }
