6e03dd3342134764f5ad8272839a4a1fcd26c765
Keith Schacht <krschacht@gmail.com>
2024-03-27 15:47:48 -0500

    Hotfix: Make MarkdownRenderer thread safe for testing

--- lib/markdown_renderer.rb
+++ lib/markdown_renderer.rb
@@ -3,17 +3,20 @@ require "./lib/redcarpet/custom_html_renderer"
 class MarkdownRenderer
 
   def self.render(markdown, options = {})
+    render_class = Redcarpet::CustomHtmlRenderer
+
     block_code_proc = options.delete(:block_code)
     if block_code_proc
-      Redcarpet::CustomHtmlRenderer.instance_eval do
+      render_class = Class.new(Redcarpet::CustomHtmlRenderer)
+      render_class.instance_eval do
         define_method(:block_code) do |code, language|
           block_code_proc.call(code, language)
         end
       end
     end
 
-    @@renderer ||= Redcarpet::CustomHtmlRenderer.new(safe_links_only: true)
-    @@formatter ||= Redcarpet::Markdown.new(@@renderer,
+    renderer = render_class.new(safe_links_only: true)
+    formatter = Redcarpet::Markdown.new(renderer,
       autolink: true,
       tables: true,
       space_after_headers: true,
@@ -27,7 +30,7 @@ class MarkdownRenderer
 
     markdown = ensure_newline_before_code_block_start(markdown)
 
-    @@formatter.render(markdown)
+    formatter.render(markdown)
   end
 
   def self.ensure_newline_before_code_block_start(markdown)
