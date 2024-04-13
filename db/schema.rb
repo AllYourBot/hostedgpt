b08f9e708ef750b254ec500ef3393058d2e1ae3c
Keith Schacht <krschacht@gmail.com>
2024-04-13 17:25:21 -0500

    New fixtures for versioned messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_12_200516) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_12_205836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -106,11 +106,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_12_200516) do
     t.datetime "rerequested_at"
     t.datetime "cancelled_at"
     t.datetime "processed_at", precision: nil
+    t.integer "index", null: false
+    t.integer "version", null: false
     t.index ["assistant_id"], name: "index_messages_on_assistant_id"
     t.index ["content_document_id"], name: "index_messages_on_content_document_id"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["index"], name: "index_messages_on_index"
     t.index ["run_id"], name: "index_messages_on_run_id"
     t.index ["updated_at"], name: "index_messages_on_updated_at"
+    t.index ["version"], name: "index_messages_on_version"
   end
 
   create_table "notes", force: :cascade do |t|
