949dfada3e800dab43a28fb88049068cf1002786
Jason Schulz <jason@schulz.studio>
2024-04-18 10:38:07 -0400

    * fix(messages_helper.rb): revert formatted_block_code method name to block_code * fix(messages_helper.rb): remove unnecessary empty line * fix(custom_html_renderer.rb): remove unused initialize method * fix(custom_html_renderer.rb): remove unnecessary empty line

--- lib/redcarpet/custom_html_renderer.rb
+++ lib/redcarpet/custom_html_renderer.rb
@@ -1,7 +1,4 @@
 class Redcarpet::CustomHtmlRenderer < Redcarpet::Render::HTML
-  def initialize(options={})
-    super(options.merge(filter_html: false))
-  end
   def paragraph(text)
     text.gsub!("\n", "<br>\n")
     "\n<p>#{text}</p>\n"
