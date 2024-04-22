61e09bd4ac612728f20f5ee22620f7b2b272162b
Jason Schulz <jason@schulz.studio>
2024-04-22 17:10:31 -0400

    PR comment resolution completes Allow user to toggle dark/light mode via settings #169

---
+++ app/javascript/controllers/dark_mode_controller.js
@@ -0,0 +1,44 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ['document'];
+  static values = { userPreference: String };
+
+  initialize() {
+    this.setDarkMode(this.userPreferenceValue);
+    this.addDarkModeListener();
+  }
+
+  toggleDarkMode = (darkMode) => {
+    if (this.hasDocumentTarget) {
+      this.documentTarget.classList.toggle('dark', darkMode);
+      this.documentTarget.classList.toggle('light', !darkMode);
+    }
+  };
+
+  addDarkModeListener() {
+    if (this.userPreferenceValue === 'system') {
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', (e) => {
+          this.toggleDarkMode(e.matches);
+        });
+    }
+  }
+
+  setDarkMode(userPreference) {
+    switch (userPreference) {
+      case 'light':
+        this.toggleDarkMode(false);
+        break;
+      case 'dark':
+        this.toggleDarkMode(true);
+        break;
+      default:
+        const prefersDarkMode = window.matchMedia(
+          '(prefers-color-scheme: dark)'
+        ).matches;
+        this.toggleDarkMode(prefersDarkMode);
+    }
+  }
+}
