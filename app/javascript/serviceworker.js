ff39786991f61ce4f4939ab04a9c1c88c290a422
Keith Schacht <krschacht@gmail.com>
2024-04-23 17:29:04 -0500

    remove JS semicolons

--- app/javascript/serviceworker.js
+++ app/javascript/serviceworker.js
@@ -1,26 +1,26 @@
-import './serviceworker';
+import './serviceworker'
 
 if ('serviceWorker' in navigator && navigator !== null) {
-  navigator.serviceWorker.register('/serviceworker.js');
+  navigator.serviceWorker.register('/serviceworker.js')
 }
 
 self.addEventListener('install', (event) => {
   // Perform install steps
   event.waitUntil(
     caches.open('static').then(function (cache) {
-      return cache.addAll(['/', '/index.html', '/css/style.css', '/js/app.js']);
+      return cache.addAll(['/', '/index.html', '/css/style.css', '/js/app.js'])
     })
-  );
-});
+  )
+})
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then(function (response) {
       // Cache hit - return response
       if (response) {
-        return response;
+        return response
       }
-      return fetch(event.request);
+      return fetch(event.request)
     })
-  );
-});
+  )
+})
