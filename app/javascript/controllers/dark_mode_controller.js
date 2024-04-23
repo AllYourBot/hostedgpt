b1a488a171da1bbe8794dedb738a7acfaab941e0
Jason Schulz <jason@schulz.studio>
2024-04-23 10:02:36 -0400

    * fix(dark_mode_controller.js): remove semicolons and fix toggleDarkMode method to use element instead of documentTarget * fix(application.html.erb): remove data-dark-mode-target attribute

--- app/javascript/controllers/dark_mode_controller.js
+++ app/javascript/controllers/dark_mode_controller.js
@@ -1,44 +1,44 @@
-import { Controller } from '@hotwired/stimulus';
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['document'];
-  static values = { userPreference: String };
+  static targets = ['document']
+  static values = { userPreference: String }
 
   connect() {
-    this.setDarkMode(this.userPreferenceValue);
-    this.addDarkModeListener();
+    this.setDarkMode(this.userPreferenceValue)
+    this.addDarkModeListener()
   }
 
   toggleDarkMode = (darkMode) => {
-    if (this.hasDocumentTarget) {
-      this.documentTarget.classList.toggle('dark', darkMode);
-      this.documentTarget.classList.toggle('light', !darkMode);
+    if (this.hasElement) {
+      this.element.classList.toggle('dark', darkMode)
+      this.element.classList.toggle('light', !darkMode)
     }
-  };
+  }
 
   addDarkModeListener() {
     if (this.userPreferenceValue === 'system') {
       window
         .matchMedia('(prefers-color-scheme: dark)')
         .addEventListener('change', (e) => {
-          this.toggleDarkMode(e.matches);
-        });
+          this.toggleDarkMode(e.matches)
+        })
     }
   }
 
   setDarkMode(userPreference) {
     switch (userPreference) {
       case 'light':
-        this.toggleDarkMode(false);
-        break;
+        this.toggleDarkMode(false)
+        break
       case 'dark':
-        this.toggleDarkMode(true);
-        break;
+        this.toggleDarkMode(true)
+        break
       default:
         const prefersDarkMode = window.matchMedia(
           '(prefers-color-scheme: dark)'
-        ).matches;
-        this.toggleDarkMode(prefersDarkMode);
+        ).matches
+        this.toggleDarkMode(prefersDarkMode)
     }
   }
 }
