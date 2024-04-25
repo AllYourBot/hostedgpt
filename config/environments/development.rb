65701c229735303dc44553b4dd7cf14fc5f186d3
Jason Schulz <jason@schulz.studio>
2024-04-25 15:33:58 -0400

    * chore(development.rb): add "hostedgpt.*." to the list of allowed hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,4 +77,5 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.hosts << /[a-z0-9\-]+\.ngrok-free\.app/
+  config.hosts << /hostedgpt.*/
 end
