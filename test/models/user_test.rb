22a93065e38a5d9cc2f0e2294cbb4f3f4bec37e1
Jason Schulz <jason@schulz.studio>
2024-04-29 15:16:29 -0400

    * fix(user.rb): set default preferences before creating a user * fix(_form.html.erb): add gap between flex items in dark mode color theme section * test(user_test.rb): remove dark_mode preference from boolean values test, add test for default and updating dark_mode preference

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -85,27 +85,44 @@ class UserTest < ActiveSupport::TestCase
 
   test "boolean values within preferences get converted back and forth properly" do
     assert_nil users(:keith).preferences[:nav_closed]
-    assert_nil users(:keith).preferences[:dark_mode]
     assert_nil users(:keith).preferences[:kids]
     assert_nil users(:keith).preferences[:city]
 
     users(:keith).update!(preferences: {
       nav_closed: true,
-      dark_mode: false,
       kids: 2,
       city: "Austin"
     })
     users(:keith).reload
+
     assert users(:keith).preferences[:nav_closed]
-    refute users(:keith).preferences[:dark_mode]
     assert_equal 2, users(:keith).preferences[:kids]
     assert_equal "Austin", users(:keith).preferences[:city]
 
     users(:keith).update!(preferences: {
       nav_closed: "false",
-      dark_mode: "true",
+
     })
+
     refute users(:keith).preferences[:nav_closed]
-    assert users(:keith).preferences[:dark_mode]
+
   end
+
+  test "dark_mode preference defaults to system and it can update user dark_mode preference" do
+    new_user = User.create!(password: 'password', first_name: 'First', last_name: 'Last')
+    assert_equal "system", new_user.preferences[:dark_mode]
+
+    new_user.update!(preferences: { dark_mode: "light" })
+    assert_equal "light", new_user.preferences[:dark_mode]
+
+    new_user.update!(preferences: { dark_mode: "dark" })
+
+    assert_equal "dark", new_user.preferences[:dark_mode]
+
+    new_user.update!(preferences: { dark_mode: "system" })
+
+    assert_equal "system", new_user.preferences[:dark_mode]
+
+  end
+
 end
