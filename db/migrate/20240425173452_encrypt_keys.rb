e5a2af116471bee7d5392e964b7875fda3c38160
Keith Schacht <krschacht@gmail.com>
2024-04-29 16:32:14 -0500

    cleanup migration

--- db/migrate/20240425173452_encrypt_keys.rb
+++ db/migrate/20240425173452_encrypt_keys.rb
@@ -1,11 +1,17 @@
 class EncryptKeys < ActiveRecord::Migration[7.1]
   def up
+    puts ""
+    puts "### ERROR? #################################################"
+    puts "### YOU SHOULD NOT RUN db:migrate INSTEAD RUN db:prepare ###"
+    puts "############################################################"
+    puts ""
+
     User.find_each do |user|
       Rails.logger.info "Encrypt keys for #{user.id}. Has openai_key: #{user.openai_key.present?}; has anthropic_key: #{user.anthropic_key.present?}"
       user.encrypt
       if !user.save
         Rails.logger.warn "Could not update user #{user.id}: #{user.errors.full_messages.join(',')}"
-      else 
+      else
         Rails.logger.info "Successfully updated user #{user.id}"
       end
     end
