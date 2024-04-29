cb823eb9bca539800357cd82fc5a9157e6226e05
Jason Schulz <jason@jasonpaul.io>
2024-04-29 19:21:37 -0400

    Improve fonts (#310)
    Co-authored-by: Jason Schulz <jason@schulz.studio>

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         'gray': {
