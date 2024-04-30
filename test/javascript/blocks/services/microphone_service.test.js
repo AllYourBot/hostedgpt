47f5a84df51d6ca2e0d0d235feff85b3e8af3713
Keith Schacht <krschacht@gmail.com>
2024-04-29 20:18:59 -0500

    Add MicrophoneService

---
+++ test/javascript/blocks/services/microphone_service.test.js
@@ -0,0 +1,48 @@
+test('start() initializes active and volume values', async() => {
+  microphone = new MicrophoneService
+  expect(microphone.$.active).toBeUndefined()
+  expect(microphone.volume).toBeUndefined()
+  await microphone.start()
+  expect(microphone.$.active).toStrictEqual(true)
+  expect(microphone.volume).toBe(0)
+})
+
+describe('tests on an instance', () => {
+  let microphone
+
+  beforeEach(async() => {
+    microphone = new MicrophoneService()
+    await microphone.start()
+  })
+
+  test('stop() after start makes things inactive', async() => {
+    expect(microphone.$.active).toStrictEqual(true)
+    microphone.stop()
+    expect(microphone.$.active).toStrictEqual(false)
+  })
+
+  test('start() still works after a stop()', async() => {
+    microphone.stop()
+    expect(microphone.$.active).toStrictEqual(false)
+    await microphone.start()
+    expect(microphone.$.active).toStrictEqual(true)
+  })
+
+  test('onaudioprocess sets the volume by calling processVolume', async() => {
+    expect(microphone.volume).toBe(0)
+
+    const mockVolumeEvent = new AudioProcessingEvent() // mocked in test_helper
+    microphone.$.audioProcessor.onaudioprocess(mockVolumeEvent)
+
+    expect(microphone.volume).toBe(4)
+  })
+
+  test('onVolumeChange fires when the volume changes sets the volume by calling processVolume', async() => {
+    expect(microphone.volume).toBe(0)
+
+    const mockVolumeEvent = new AudioProcessingEvent() // mocked in test_helper
+    microphone.$.audioProcessor.onaudioprocess(mockVolumeEvent)
+
+    expect(microphone.volume).toBe(4)
+  })
+})
