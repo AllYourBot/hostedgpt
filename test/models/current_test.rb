a5be3ac742e6943e5031a27cb7c716ca026ecf87
Jason Schulz <jason@schulz.studio>
2024-05-02 11:25:30 -0400

    * chore(current_test.rb): rename current_test.rb to test/models/current_test.rb * test(current_test.rb): update test descriptions and refactor code

--- test/models/current_test.rb
+++ test/models/current_test.rb
@@ -1,24 +1,24 @@
 require 'test_helper'
 
 class CurrentTest < ActiveSupport::TestCase
-  test "user method sets default preferences" do
+  test "user dark mode preference will be set to 'system' if preference does not exist" do
     user = User.new
-    current = Current.new(user)
+    Current.user = user
 
     assert_nil user.preferences[:dark_mode]
-    current.user
+
+    Current.user
 
     assert_equal 'system', user.preferences[:dark_mode]
   end
 
-  test "user method does not override existing preferences" do
+  test "user dark mode preference is not overridden" do
     user = User.new
     user.preferences = { dark_mode: 'light' }
-    current = Current.new(user)
+    Current.user = user
 
-    current.user
+    Current.user
 
     assert_equal 'light', user.preferences[:dark_mode]
   end
-
 end
