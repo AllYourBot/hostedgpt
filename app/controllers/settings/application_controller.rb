06de134b2d6abecdb36b916c39ba5660acfa0e10
Stephan Wehner <stephan@buckmaster.ca>
2024-05-09 18:14:23 -0700

    Load all assistants when editing, only show 5 initially

--- app/controllers/settings/application_controller.rb
+++ app/controllers/settings/application_controller.rb
@@ -1,21 +1,21 @@
 class Settings::ApplicationController < ApplicationController
   layout "settings"
-  before_action :set_settings_menu
+  before_action :set_settings_assistants
 
   private
 
-  def set_settings_menu
-    # controller_name => array of items
-    @settings_menu = {
-      assistants: Current.user.assistants.ordered.map {
+  def set_settings_assistants
+    settings_assistants = Current.user.assistants.ordered.map {
         |assistant| [ assistant, edit_settings_assistant_path(assistant) ]
-      }.to_h.merge({
-        'New Assistant': new_settings_assistant_path
-      }),
-
-      people: {
-        'Account': edit_settings_person_path
       }
-    }
+    @settings_assistants = settings_assistants[0,5]
+    if settings_assistants.length > 5
+      @hide_settings_assistants = settings_assistants[5, settings_assistants.length - 5]
+
+      # If user is editing an "overflow assistant" don't collapse that section, keep it open from the start
+      @unhide_settings_assistants = params[:controller] =='settings/assistants' &&
+        params[:action] == 'edit' &&
+        @hide_settings_assistants.map(&:first).map(&:id).include?(params[:id].to_i)
+    end
   end
 end
