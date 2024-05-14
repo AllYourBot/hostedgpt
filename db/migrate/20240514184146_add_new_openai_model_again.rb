41b6e2d14e4fe0672ba85707ce090f7fd99b6666
Keith Schacht <krschacht@gmail.com>
2024-05-14 09:39:33 -0500

    Hotfix: Migrate GPT-4 again to GPT-4o

---
+++ db/migrate/20240514184146_add_new_openai_model_again.rb
@@ -0,0 +1,9 @@
+class AddNewOpenaiModelAgain < ActiveRecord::Migration[7.1]
+  def up
+    Assistant.where(model: "gpt-4-turbo-2024-04-09").update_all(model: "gpt-4o-2024-05-13")
+  end
+
+  def down
+    Assistant.where(model: "gpt-4o-2024-05-13").update_all(model: "gpt-4-turbo-2024-04-09")
+  end
+end
