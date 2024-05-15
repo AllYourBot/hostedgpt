c0803af9708eebca9bad5227aaed68f0bd56fbb4
Matthew Bennink <matthew.bennink@soomolearning.com>
2024-05-15 17:22:43 +0200

    [whitespace] fix indentation issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,19 +6,19 @@ class ApplicationController < ActionController::Base
   after_action { response.headers.except! 'X-Frame-Options' }
 
   def launch
-	person = Person.create!(
-		personable_type: 'User',
-		personable_attributes: {
-			password: (@password = SecureRandom.uuid),
-			name: 'Soomo User',
-			openai_key: ENV['SOOMO_OPENAI_KEY'],
-			anthropic_key: ENV['SOOMO_ANTHROPIC_KEY']
-		},
-		email: "#{@password}@hostedgpt.soomo"
-	)
-	reset_session
+    person = Person.create!(
+      personable_type: 'User',
+      personable_attributes: {
+        password: (@password = SecureRandom.uuid),
+        name: 'Soomo User',
+        openai_key: ENV['SOOMO_OPENAI_KEY'],
+        anthropic_key: ENV['SOOMO_ANTHROPIC_KEY']
+      },
+      email: "#{@password}@hostedgpt.soomo"
+    )
+    reset_session
     login_as person.user
 
-	render json: { assistantId: person.user.assistants.ordered.first.id }
+    render json: { assistantId: person.user.assistants.ordered.first.id }
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -84,14 +84,14 @@ Rails.application.configure do
   config.action_cable.allowed_request_origins = [/webtexts/]
 
   config.middleware.insert_before 0, Rack::Cors do
-	allow do
-	  origins /webtexts/
+    allow do
+      origins /webtexts/
 
-	  resource "*",
-		headers: :any,
-		methods: [:get, :post, :put, :patch, :delete, :options, :head],
-		credentials: true,
-		max_age: 86400
-	end
+      resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true,
+      max_age: 86400
+    end
   end
 end
