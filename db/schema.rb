249d3ad59741a2b7de617be341df9134ff3cc7e8
Keith Schacht <krschacht@gmail.com>
2024-05-19 13:41:36 -0500

    Hotfix: Correct broken schema file

--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_14_184146) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "images", default: false, null: false
+    t.string "model"
     t.index ["user_id"], name: "index_assistants_on_user_id"
   end
 
