5c3a24046900efb270db99be1ce77f8a081432d1
Keith Schacht <krschacht@gmail.com>
2024-05-20 20:16:05 -0500

    Fix some race conditions with speach playback

--- app/javascript/stimulus/speaker_controller.js
+++ app/javascript/stimulus/speaker_controller.js
@@ -9,7 +9,13 @@ export default class extends Controller {
     this.textTargetsCount = this.textTargets.length
     this.thoughtsSentCount = 0
 
-    this.textTargets.forEach((target) => target.addEventListener('turbo:before-morph', this.boundParseWords))
+    this.textTargets.forEach((target) => target.addEventListener('turbo:before-morph-element', this.boundParseWords))
+    document.addEventListener('turbo:visit', this.lastParseWords)
+  }
+
+  disconnect() {
+    this.textTargets.forEach((target) => target.removeEventListener('turbo:before-morph-element', this.boundParseWords))
+    document.removeEventListener('turbo:visit', this.lastParseWords)
   }
 
   textTargetConnected(target) {
@@ -25,12 +31,10 @@ export default class extends Controller {
     this.parseWords(target)
   }
 
-  disconnect() {
-    this.textTargets.forEach((target) => target.removeEventListener('turbo:before-morph', this.boundParseWords))
-  }
-
-  boundParseWords = (event) => { this.parseWords(event) }
-  parseWords(target) {
+  lastParseWords = () => { this.parseWords(this.textTargets.last(), 'visit replace') }
+  boundParseWords = (event) => { this.parseWords(event.target, 'morph-element') }
+  parseWords(target, source = 'targetConnected') {
+    if (!target) return
     if (Microphone.off) return
 
     const thinking = target.getAttribute('data-thinking') === 'true'
@@ -39,7 +43,9 @@ export default class extends Controller {
     for(this.thoughtsSentCount; this.thoughtsSentCount < thoughts.length-1; this.thoughtsSentCount ++)
       Prompt.Speaker.toSay(thoughts[this.thoughtsSentCount])
 
-    if (!thinking && this.thoughtsSentCount == thoughts.length-1)
+    if (!thinking && this.thoughtsSentCount == thoughts.length-1) {
       Prompt.Speaker.toSay(thoughts[this.thoughtsSentCount])
+      this.thoughtsSentCount += 1
+    }
   }
 }
