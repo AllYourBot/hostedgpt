c5071448c7c5cd8546efcd716aeb68643c73060e
Rich Mpanga <rmpanga@gmail.com>
2024-05-21 13:15:59 -0400

    Small fixes

--- db/migrate/20240520195255_add_external_id_to_conversation.rb
+++ db/migrate/20240520195255_add_external_id_to_conversation.rb
@@ -2,7 +2,6 @@ class AddExternalIdToConversation < ActiveRecord::Migration[7.1]
   def up
     add_column :conversations, :external_id, :text, if_not_exists: true,  comment: "The Backend AI system (e.g OpenAI) Thread Id"
     add_index  :conversations, :external_id, unique: true, if_not_exists: true
-
   end
 
   def down
