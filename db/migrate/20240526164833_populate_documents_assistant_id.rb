27ec521443f9389411f7869b812422e58ca3816e
Keith Schacht <krschacht@gmail.com>
2024-05-27 17:28:56 -0500

    Update migrations

--- db/migrate/20240526164833_populate_documents_assistant_id.rb
+++
@@ -1,9 +0,0 @@
-class PopulateDocumentsAssistantId < ActiveRecord::Migration[7.1]
-  def up
-    ActiveRecord::Base.connection.execute "update documents docs set assistant_id = (select assistant_id from messages msgs where msgs.id = docs.message_id) where docs.assistant_id is null"
-  end
-
-  def down
-    say "No change"
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_26_164833) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_144314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
