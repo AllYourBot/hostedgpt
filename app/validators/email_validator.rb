79b54415f3e98bdf41bd51bea9aa86822c2eb200
Tobias Ludwig Maier <me@tobiasmaier.info>
2024-06-03 13:51:49 +0200

    Remove EmailValidator

--- app/validators/email_validator.rb
+++
@@ -1,7 +0,0 @@
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless URI::MailTo::EMAIL_REGEXP.match?(value)
-      record.errors.add attribute, (options[:message] || "is not an email")
-    end
-  end
-end
