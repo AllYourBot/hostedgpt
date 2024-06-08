b05cc63782941115f141c8975b997bbf8fb66fb7
Keith Schacht <krschacht@gmail.com>
2024-06-08 06:50:12 -0500

    Add a forced logout test

--- test/controllers/authentications_controller_test.rb
+++ test/controllers/authentications_controller_test.rb
@@ -12,51 +12,19 @@ class AuthenticationsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create session" do
-    post login_path, params: {
-      email: people(:keith_registered).email,
-      password: "secret"
-    }
-    assert_redirected_to root_url # we are not actually checking that a valid session was created?
-  end
-
-  test "should ignore capitalization of email and create a session" do
-    post login_path, params: {
-      email: people(:keith_registered).email.capitalize,
-      password: "secret"
-    }
-    assert_redirected_to root_url
-  end
-
-  test "should ignore whitespace around an email addresss and create a session" do
-    post login_path, params: {
-      email: " " + people(:keith_registered).email.capitalize,
-      password: "secret"
-    }
-    assert_redirected_to root_url
-  end
-
-  test "should redirect back after invalid password" do
-    post login_path, params: {
-      email: people(:keith_registered).email,
-      password: "wrong"
-    }
-    assert_response :unprocessable_entity
-    assert_match(/Invalid email or password/, flash.alert)
-  end
-
-  test "should redirect back after invalid email" do
-    post login_path, params: {
-      email: "wrong@email.com",
-      password: "wrong"
-    }
-    assert_response :unprocessable_entity
-    assert_match(/Invalid email or password/, flash.alert)
-  end
-
   test "visiting logout should logout someone who was logged in" do
     login_as people(:keith_registered)
     get logout_path
+    assert_redirected_to login_url
+  end
+
+  test "when logged in and the authentication is deleted then the user should be logged out" do
+    login_as people(:keith_registered)
+    get new_assistant_message_path(assistants(:samantha))
+    assert_response :success
+
+    users(:keith).password_credential.authentications.map(&:deleted!)
+    get new_assistant_message_path(assistants(:samantha))
     assert_redirected_to login_url
   end
 
