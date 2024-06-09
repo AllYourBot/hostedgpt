3967eb0795b40b2e422fadfd10888ecd02482195
Keith Schacht <krschacht@gmail.com>
2024-06-09 11:59:39 -0500

    Revert "create default Language Models for registration"
    This reverts commit 876503b3ff9ed0c1c54ed1891ae03c6dbeef8c7b.

--- app/models/concerns/user/registerable.rb
+++ app/models/concerns/user/registerable.rb
@@ -8,13 +8,9 @@ module User::Registerable
   private
 
   def create_initial_assistants
-    language_model = LanguageModel.find_by(name: 'gpt-4o') || LanguageModel.create!(position: 1, supports_images: true, description: "gpt-4o")
-    assistants.create! name: "GPT-4o", language_model: language_model
-    language_model = LanguageModel.find_by(name: 'gpt-3.5-turbo') || LanguageModel.create!(position: 2, supports_images: false, description: "gpt-3.5-turbo")
-    assistants.create! name: "GPT-3.5", language_model: language_model
-    language_model = LanguageModel.find_by(name: 'claude-3-opus-20240229') || LanguageModel.create!(position: 3, supports_images: true, description: "claude-3-opus-20240229")
-    assistants.create! name: "Claude 3 Opus", language_model: language_model
-    language_model = LanguageModel.find_by(name: 'claude-3-sonnet-20240229') || LanguageModel.create!(position: 4, supports_images: true, description: "claude-3-sonnet-20240229")
-    assistants.create! name: "Claude 3 Sonnet", language_model: language_model
+    assistants.create! name: "GPT-4o", language_model: LanguageModel.find_by(name: 'gpt-4o')
+    assistants.create! name: "GPT-3.5", language_model: LanguageModel.find_by(name: 'gpt-3.5-turbo')
+    assistants.create! name: "Claude 3 Opus", language_model: LanguageModel.find_by(name: 'claude-3-opus-20240229')
+    assistants.create! name: "Claude 3 Sonnet", language_model: LanguageModel.find_by(name: 'claude-3-sonnet-20240229')
   end
 end
