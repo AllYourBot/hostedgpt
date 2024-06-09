015fbb2d77dbc99357d71fb8db9be1eee2a2495e
Keith Schacht <krschacht@gmail.com>
2024-06-09 12:47:41 -0500

    revert seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,11 @@
 require "active_record/fixtures"
 
-unless Rails.env.test?
-  puts "loading fixtures"
+puts "loading fixtures"
 
-  order_to_load_fixtures = %w[language_models]
+order_to_load_fixtures = %w[language_models]
 
-  ActiveRecord::Base.transaction do
-    ActiveRecord::Base.connection.disable_referential_integrity do
-      ActiveRecord::FixtureSet.create_fixtures(Rails.root.join('test', 'fixtures'), order_to_load_fixtures)
-    end
+ActiveRecord::Base.transaction do
+  ActiveRecord::Base.connection.disable_referential_integrity do
+    ActiveRecord::FixtureSet.create_fixtures(Rails.root.join('test', 'fixtures'), order_to_load_fixtures)
   end
 end
