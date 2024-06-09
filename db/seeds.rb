a90037db0563e67ee88d5e7654c7b9bee20bbb91
Keith Schacht <krschacht@gmail.com>
2024-06-09 13:03:53 -0500

    Hotfix: Properly run db:seeds in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
 require "active_record/fixtures"
 
-puts "loading fixtures"
+unless Rails.env.test?
+  puts "loading fixtures"
 
-order_to_load_fixtures = %w[language_models]
+  order_to_load_fixtures = %w[language_models]
 
-ActiveRecord::Base.transaction do
-  ActiveRecord::Base.connection.disable_referential_integrity do
-    ActiveRecord::FixtureSet.create_fixtures(Rails.root.join('test', 'fixtures'), order_to_load_fixtures)
+  ActiveRecord::Base.transaction do
+    if Rails.env.production?
+      ActiveRecord::FixtureSet.create_fixtures(Rails.root.join('test', 'fixtures'), order_to_load_fixtures)
+    else
+      ActiveRecord::Base.connection.disable_referential_integrity do
+        ActiveRecord::FixtureSet.create_fixtures(Rails.root.join('test', 'fixtures'), order_to_load_fixtures)
+      end
+    end
   end
 end
