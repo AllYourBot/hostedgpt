a85f5655f6cdb8b3f440017f711652c72cfa2ed6
Keith Schacht <krschacht@gmail.com>
2024-06-09 08:18:42 -0500

    Encrypt fields

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -1,5 +1,6 @@
 class Credential < ApplicationRecord
   belongs_to :user
+  encrypts :external_id, :oauth_email, :oauth_token, :oauth_refresh_token, deterministic: true
 
   has_many :authentications, -> { not_deleted }
   has_many :authentications_including_deleted, class_name: "Authentication", inverse_of: :credential, dependent: :destroy
