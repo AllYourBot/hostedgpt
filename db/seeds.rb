e2dd0616dbebebe37e6b9c9ae72b7953a0610e62
Keith Schacht <krschacht@gmail.com>
2024-06-09 14:53:20 -0500

    Hotfix: Another attempt to fix prod seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,23 @@
 require "active_record/fixtures"
 
+if Rails.env.production?
+  module ActiveRecord
+    module ConnectionAdapters
+      class PostgreSQLAdapter
+        def disable_referential_integrity
+          yield
+        end
+      end
+    end
+  end
+end
+
 unless Rails.env.test?
   puts "loading fixtures"
 
   order_to_load_fixtures = %w[language_models]
 
   ActiveRecord::Base.transaction do
-    ActiveRecord::FixtureSet.create_fixtures(Rails.root.join('test', 'fixtures'), order_to_load_fixtures)
+    ActiveRecord::FixtureSet.create_fixtures(Rails.root.join("test", "fixtures"), order_to_load_fixtures)
   end
 end
