ca8ac5ddf75ec6e60b618da9ffe15caaee969a5d
Matthew Bennink <matthew.bennink@soomolearning.com>
2024-06-10 12:54:36 +0200

    whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "assistants#index"
 
-	post '/soomo/launch' => 'application#launch'
+  post '/soomo/launch' => 'application#launch'
 
   resources :users, only: [:new, :create, :update]
 
