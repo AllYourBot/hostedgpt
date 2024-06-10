6f6a996787fbbd085fd8a54e9b76e111d2642dc3
Keith Schacht <krschacht@gmail.com>
2024-06-10 07:24:56 -0500

    Hotfix: Another attempt to auto-load production fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,22 +2,26 @@ require "active_record/fixtures"
 
 if Rails.env.production?
   module ActiveRecord
-    module ConnectionAdapters
-      class PostgreSQLAdapter
-        def disable_referential_integrity
-          yield
-        end
+    class FixtureSet
+      def self.check_all_foreign_keys_valid!(*args)
+        puts "Skipping referential integrity check for foreign"
       end
     end
   end
 end
 
 unless Rails.env.test?
-  puts "loading fixtures"
+  puts "Loading fixtures"
 
   order_to_load_fixtures = %w[language_models]
 
   ActiveRecord::Base.transaction do
+    begin
     ActiveRecord::FixtureSet.create_fixtures(Rails.root.join("test", "fixtures"), order_to_load_fixtures)
+
+    rescue => e
+      binding.pry
+
+    end
   end
 end
