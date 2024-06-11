685dda6a47b75059b0a7428d0b3871c3cea50ec8
Stephan Wehner <stephan@buckmaster.ca>
2024-06-11 12:03:31 -0700

    Fix indentation

--- db/migrate/20240526195302_expand_language_models.rb
+++ db/migrate/20240526195302_expand_language_models.rb
@@ -1,8 +1,8 @@
 class ExpandLanguageModels < ActiveRecord::Migration[7.1]
   def change
-      add_column :language_models, :user_id, :bigint, null: true
-      add_column :language_models, :deleted_at, :timestamp, default: nil
+    add_column :language_models, :user_id, :bigint, null: true
+    add_column :language_models, :deleted_at, :timestamp, default: nil
 
-      add_index :language_models, [:user_id, :deleted_at]
+    add_index :language_models, [:user_id, :deleted_at]
   end
 end
