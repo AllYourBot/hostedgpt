228dcb490dee6ff4cfb4420f2d6b20d7b7cb8eca
Stephan Wehner <stephan@buckmaster.ca>
2024-06-13 10:29:57 -0700

    Move the TestClients out of the app directory, to test/support

---
+++ test/support/test_client/anthropic.rb
@@ -0,0 +1,48 @@
+module TestClient
+  class Anthropic
+    attr_reader :uri_base
+    def initialize(access_token:, uri_base:nil)
+      @uri_base = uri_base
+    end
+  
+    def self.text
+      nil
+    end
+  
+    # This response is a valid example response from the API.
+    #
+    # Stub this method to respond with something more specific if needed.
+    def messages(**args)
+      model = args.dig(:model) || "no model"
+      system_message = args.dig(:system)
+      if proc = args.dig(:parameters, :stream)
+        proc.call({
+          "id"=>"msg_01LtHY4sJVd7WBdPCsYb8kHQ",
+          "type"=>"message",
+          "role"=>"assistant",
+          "delta"=>
+            {"type"=>"text",
+              "text"=> self.class.text || "Hello this is model #{model} with instruction #{system_message.to_s.inspect}! How can I assist you today?"},
+          "model"=>model,
+          "stop_reason"=>"end_turn",
+          "stop_sequence"=>nil,
+          "usage"=>{"input_tokens"=>10, "output_tokens"=>19}
+        })
+      else
+        {
+          "id"=>"msg_01LtHY4sJVd7WBdPCsYb8kHQ",
+          "type"=>"message",
+          "role"=>"assistant",
+          "content"=>
+            [{"type"=>"text",
+              "text"=> self.class.text || "Hello this is model #{model} with instruction #{system_message.to_s.inspect}! How can I assist you today?"}],
+          "model"=> model,
+          "stop_reason"=>"end_turn",
+          "stop_sequence"=>nil,
+          "usage"=>{"input_tokens"=>10, "output_tokens"=>19}
+  
+        }.dig("content", 0, "text")
+      end
+    end
+  end
+end
