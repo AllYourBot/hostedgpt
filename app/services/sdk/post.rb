8d7d44f85b7a756195b659c181bea3049b7ece26
Keith Schacht <krschacht@gmail.com>
2024-06-17 09:13:13 -0500

    Refactor SDK

--- app/services/sdk/post.rb
+++ app/services/sdk/post.rb
@@ -1,10 +1,20 @@
 class SDK::Post < SDK::Verb
-  def params(params = {})
-    response = Faraday.post(@url) do |req|
-      req.headers["Authorization"] = "Bearer #{@bearer_token_proc.call}" if @bearer_token_proc.call
-      req.body = better_to_hash(params).to_json
+  def param(params = {})
+    hash = OpenData.new(params).to_h
+    if @headers[:"Content-Type"] == "application/x-www-form-urlencoded"
+      body = hash.to_query
+    else
+      body = hash.to_json
     end
-    raise "Unexpected response: #{response.status} - #{response.body}" if response.status != 200
+
+    response = Faraday.post(@url) do |req|
+      req.headers = @headers
+      req.body = body
+    end
+
+    raise "Unexpected response: #{response.status} - #{response.body}" if !response.status.in? @expected_statuses
+    return response if response.status != 200
+
     JSON.parse(response.body, object_class: OpenStruct)
   end
 end
