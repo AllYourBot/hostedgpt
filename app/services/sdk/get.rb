0529fcd9e104433eae51ba767f75abe379f29824
Keith Schacht <krschacht@gmail.com>
2024-06-17 21:57:34 -0500

    Improve Fly config: add rake task for DB swap size (#421)

--- app/services/sdk/get.rb
+++ app/services/sdk/get.rb
@@ -1,6 +1,8 @@
 class SDK::Get < SDK::Verb
   def param(params = {})
     hash = OpenData.new(params).to_h
+    raise "Url contains a question mark, put that in params instead" if @url.include?("?")
+
     response = Faraday.get(@url + "?" + hash.to_query) do |req|
       req.headers = @headers
     end
