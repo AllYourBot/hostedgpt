aab38d6749c3c301bc5051a2c827c5c5a2a01920
Keith Schacht <krschacht@gmail.com>
2024-06-20 16:29:46 -0400

    Add Claude 3.5 Sonnet and make it the Anthropic default (#427)

--- app/models/user/registerable.rb
+++ app/models/user/registerable.rb
@@ -10,7 +10,7 @@ module User::Registerable
   def create_initial_assistants
     assistants.create! name: "GPT-4o", language_model: LanguageModel.find_by(name: "gpt-best")
     assistants.create! name: "GPT-3.5", language_model: LanguageModel.find_by(name: "gpt-3.5-turbo")
-    assistants.create! name: "Claude 3 Opus", language_model: LanguageModel.find_by(name: "claude-best")
-    assistants.create! name: "Claude 3 Sonnet", language_model: LanguageModel.find_by(name: "claude-3-sonnet-20240229")
+    assistants.create! name: "Claude 3.5 Sonnet", language_model: LanguageModel.find_by(name: "claude-best")
+    assistants.create! name: "Claude 3 Opus", language_model: LanguageModel.find_by(name: "claude-3-opus-20240229")
   end
 end
