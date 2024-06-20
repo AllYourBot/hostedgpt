e847b92608b079af6f5d4a24dbb6e7d0cab3cbeb
Keith Schacht <krschacht@gmail.com>
2024-06-20 16:06:41 -0400

    fix migration

--- db/migrate/20240620100000_add_claude_3_5_sonnet_to_language_models.rb
+++ db/migrate/20240620100000_add_claude_3_5_sonnet_to_language_models.rb
@@ -14,7 +14,9 @@ class AddClaude35SonnetToLanguageModels < ActiveRecord::Migration[7.0]
     end
 
     Assistant.where(name: "Claude 3 Opus").update_all(name: "Claude 3.5 Sonnet")
-    Assistant.where(name: "Claude 3 Sonnet").update_all(name: "Claude 3 Opus", language_model_id: LanguageModel.find_by(name: "claude-3-opus-20240229").id)
+    if language_model_id = LanguageModel.find_by(name: "claude-3-opus-20240229")&.id
+      Assistant.where(name: "Claude 3 Sonnet").update_all(name: "Claude 3 Opus", language_model_id: language_model_id)
+    end
   end
 
   def down
