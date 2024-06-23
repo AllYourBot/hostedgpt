b7ccb75ff5cf64a4f32c4320885865211ca5b010
Keith Schacht <krschacht@gmail.com>
2024-06-23 16:30:16 -0500

    wip

--- db/migrate/20240526195302_expand_language_models.rb
+++ db/migrate/20240526195302_expand_language_models.rb
@@ -4,6 +4,7 @@ class ExpandLanguageModels < ActiveRecord::Migration[7.1]
 
     rename_column :language_models, :name, :api_name
     change_column_comment :language_models, :api_name, "This is the name that API calls are expecting."
+    rename_column :language_models, :description, :name
 
     add_reference :language_models, :user, null: true, foreign_key: true, index: true
     add_index :language_models, [:user_id, :deleted_at]
