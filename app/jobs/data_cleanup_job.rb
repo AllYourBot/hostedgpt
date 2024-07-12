f865a1276f5ea6a65a1ddea0bbc9fc7729b4a6f9
Matthew Bennink <matthew.bennink@soomolearning.com>
2024-07-12 14:17:11 +0200

    use #pluck and #destroy_all

--- app/jobs/data_cleanup_job.rb
+++ app/jobs/data_cleanup_job.rb
@@ -3,16 +3,12 @@ class DataCleanupJob < ApplicationJob
   queue_as :default
 
   def perform
-    threshold = 90.days.ago
-
     user_ids = Message.
-      select('users.id as user_id, MAX(messages.created_at) as max_messages_created_at').
       joins(:conversation => :user).
       group('users.id').
-      to_a.select do |message|
-        message.max_messages_created_at < threshold
-      end.map(&:user_id)
+      having('MAX(messages.created_at) < ?', 3.days.ago).
+      pluck('users.id')
 
-    User.where(id: user_ids).each(&:destroy)
+    User.where(id: user_ids).destroy_all
   end
 end
