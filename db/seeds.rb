f1938ad6e5fee102a29f7ba52530c3b0374f4f83
Justin Vallelonga <jlvallelonga@gmail.com>
2024-07-19 13:20:20 +0700

    removes commented line

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,6 @@ require "active_record/fixtures"
 if Rails.env.development?
 
   puts "loading fixtures"
-  # order_to_load_fixtures = %w[people users tombstones language_models assistants conversations runs messages steps ]
   order_to_load_fixtures = %w[
     people
     users
