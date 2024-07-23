6ac901f23cc577300b91d8a271023c68e0012501
Justin Vallelonga <jlvallelonga@gmail.com>
2024-07-23 22:50:13 +0700

    adds browser and os lists as globals in init

--- app/jobs/send_reset_password_email_job.rb
+++ app/jobs/send_reset_password_email_job.rb
@@ -2,8 +2,8 @@ class SendResetPasswordEmailJob < ApplicationJob
   queue_as :default
 
   def perform(email, user_agent)
-    os = get_os_from_user_agent(user_agent)
-    browser = get_browser_from_user_agent(user_agent)
+    os = get_item_in_str(user_agent, KNOWN_OPERATING_SYSTEMS) || "unknown operating system"
+    browser = get_item_in_str(user_agent, KNOWN_BROWSERS) || "unknown browser"
 
     if @person = Person.find_by_email(email)
       PasswordMailer.with(person: @person, os: os, browser: browser).reset.deliver_later
@@ -12,35 +12,11 @@ class SendResetPasswordEmailJob < ApplicationJob
 
   private
 
-  def get_os_from_user_agent(user_agent)
-    if user_agent.include?("Windows")
-      "Windows"
-    elsif user_agent.include?("Macintosh")
-      "Macintosh"
-    elsif user_agent.include?("Linux")
-      "Linux"
-    elsif user_agent.include?("Android")
-      "Android"
-    elsif user_agent.include?("iPhone")
-      "iPhone"
-    else
-      "unknown operating system"
-    end
-  end
-
-  def get_browser_from_user_agent(user_agent)
-    if user_agent.include?("Chrome")
-      "Chrome"
-    elsif user_agent.include?("Safari")
-      "Safari"
-    elsif user_agent.include?("Firefox")
-      "Firefox"
-    elsif user_agent.include?("Edge")
-      "Edge"
-    elsif user_agent.include?("Opera")
-      "Opera"
-    else
-      "unknown browser"
+  def get_item_in_str(str, items)
+    items.each do |item|
+      if str.include?(item)
+        return item
+      end
     end
   end
 end
---
+++ config/initializers/user_agent.rb
@@ -0,0 +1,2 @@
+KNOWN_OPERATING_SYSTEMS = ["Windows", "Macintosh", "Linux", "Android", "iPhone"].freeze
+KNOWN_BROWSERS = ["Chrome", "Safari", "Firefox", "Edge", "Opera"].freeze
