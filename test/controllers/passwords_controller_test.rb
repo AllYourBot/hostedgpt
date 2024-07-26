0ef3581e48f6d38cb60d7b3688dee0d7f9a0766d
Justin Vallelonga <jlvallelonga@gmail.com>
2024-07-26 08:02:11 +0700

    fixes controller tests

--- test/controllers/passwords_controller_test.rb
+++ test/controllers/passwords_controller_test.rb
@@ -4,25 +4,25 @@ class PasswordsControllerTest < ActionDispatch::IntegrationTest
   include ActionDispatch::TestProcess::FixtureFile
 
   setup do
-    @person = people(:keith_registered)
-    users(:keith)
+    people(:keith_registered)
+    @user = users(:keith)
     credentials(:keith_password)
   end
 
   test "should get edit" do
-    token = get_test_person_token(@person)
+    token = get_test_user_token(@user)
 
     get edit_password_url, params: { token: token }
 
     assert_response :success
-    assert assigns(:person).is_a?(Person)
-    assert_equal @person, assigns(:person)
+    assert assigns(:user).is_a?(User)
+    assert_equal @user, assigns(:user)
   end
 
   test "should patch update" do
-    token = get_test_person_token(@person)
+    token = get_test_user_token(@user)
 
-    patch password_url, params: { token: token, person: { personable_attributes: { credentials_attributes: { "0" => { type: "PasswordCredential", password: "new_password" } } } } }
+    patch password_url, params: { token: token, password: "new_password" }
 
     assert_response :redirect
     assert_redirected_to login_path
@@ -30,8 +30,8 @@ class PasswordsControllerTest < ActionDispatch::IntegrationTest
 
   private
 
-  def get_test_person_token(person)
-    person.signed_id(
+  def get_test_user_token(user)
+    user.signed_id(
       purpose: Rails.application.config.password_reset_token_purpose,
       expires_in: Rails.application.config.password_reset_token_ttl_minutes.minutes
     )
