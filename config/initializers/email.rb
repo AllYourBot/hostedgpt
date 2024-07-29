4edfe4971fa682b9e03731c924a496707f468b0b
Justin Vallelonga <jlvallelonga@gmail.com>
2024-07-29 14:02:41 +0700

    adds constants for email senders

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,11 +1,22 @@
+class EmailSenders
+  POSTMARK = "postmark".freeze
+
+  class << self
+    def all
+      constants.map { |c| const_get(c) }
+    end
+  end
+end
+EmailSenders.freeze
+
 if Feature.email?
   Rails.application.configure do
     Setting.require_keys!(:email_sender, :email_from, :email_host)
-    Setting.require_value_in!(:email_sender, ["postmark"])
+    Setting.require_value_in!(:email_sender, EmailSenders.all)
 
     config.action_mailer.default_url_options = { host: Setting.email_host }
 
-    if Setting.email_sender == "postmark"
+    if Setting.email_sender == EmailSenders::POSTMARK
       Setting.require_keys!(:postmark_server_api_token)
 
       config.action_mailer.delivery_method = :postmark
