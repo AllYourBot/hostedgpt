735d8fd5bda0e98f8ceae198eb339ae00d8af8ec
Keith Schacht <krschacht@gmail.com>
2024-08-05 16:47:37 -0500

    set timeout a diff way

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,16 +18,22 @@ end
 class ActionDispatch::IntegrationTest
   include Rails.application.routes.url_helpers
 
-  Capybara.register_driver :selenium_with_long_timeout do |app|
-    client = Selenium::WebDriver::Remote::Http::Default.new
-    client.timeout = 120
-    client.open_timeout = 120
-    client.read_timeout = 120
-    Capybara::Driver::Selenium.new(app, :http_client => client)
-  end
-  Capybara.javascript_driver = :selenium_with_long_timeout
+  # Capybara.register_driver :selenium_with_long_timeout do |app|
+  #   client = Selenium::WebDriver::Remote::Http::Default.new(open_timeout: 180, read_timeout: 180)
+  #   Capybara::Driver::Selenium.new(app, :http_client => client)
+  # end
+  # Capybara.javascript_driver = :selenium_with_long_timeout
 
-  Capybara.default_max_wait_time = 10
+  # Capybara.default_max_wait_time = 10
+
+
+    #   def self.driven_by(driver, using: :chrome, screen_size: [1400, 1400], options: {}, &capabilities)
+    #   driver_options = { using: using, screen_size: screen_size, options: options }
+
+    #   self.driver = SystemTesting::Driver.new(driver, **driver_options, &capabilities)
+    # end
+
+
 
   def login_as(user_or_person, password = "secret")
     user = if user_or_person.is_a?(Person)
