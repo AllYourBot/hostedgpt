70745d789b01e307dc90d82c9f1f725b56d353db
Keith Schacht <krschacht@gmail.com>
2024-08-10 21:05:27 -0500

    scoping

--- config/initializers/duration_extension.rb
+++ config/initializers/duration_extension.rb
@@ -1,6 +1,6 @@
 ActiveSupport::Duration.class_eval do
   def as_sentence
-    Duration.build(self.seconds).parts.
+    ActiveSupport::Duration.build(self.seconds).parts.
       map { |unit, v| "#{v} #{v == 1 ? unit.to_s.singularize : unit.to_s}" }.
       to_sentence
   end
