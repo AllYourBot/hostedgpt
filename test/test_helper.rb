02ecb3e973fc9a6362f651c5556554a2f47ef745
Justin Vallelonga <jlvallelonga@gmail.com>
2024-09-30 12:14:32 +0700

    removes unnecessary code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,8 @@ require_relative "../config/environment"
 require "rails/test_help"
 require "minitest/autorun"
 require "pry"
-require "capybara/rails"
-require "capybara/minitest"
+# require "capybara/rails"
+# require "capybara/minitest"
 
 Dir[Rails.root.join("test/support/**/*.rb")].sort.each { |file| require file }
 
@@ -22,25 +22,25 @@ class Capybara::Node::Element
   end
 end
 
-Capybara.register_driver :headless_chrome do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-    opts.add_argument("--headless")
-    # opts.add_argument("--disable-gpu") if Gem.win_platform?
-    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
-    # opts.add_argument("--disable-site-isolation-trials")
-    # opts.add_argument("--window-size=1920,1080")
-    # opts.add_argument("--disable-search-engine-choice-screen")
-    opts.add_argument("--disable-dev-shm-usage")
-    opts.add_argument("--window-size=1400,800") # your desired window size
-    # opts.add_argument("--no-sandbox")
-    # pin browser version to 127
-    # opts.add_argument("--browser-version=127")
+# Capybara.register_driver :headless_chrome do |app|
+#   options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+#     opts.add_argument("--headless")
+#     # opts.add_argument("--disable-gpu") if Gem.win_platform?
+#     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
+#     # opts.add_argument("--disable-site-isolation-trials")
+#     # opts.add_argument("--window-size=1920,1080")
+#     # opts.add_argument("--disable-search-engine-choice-screen")
+#     opts.add_argument("--disable-dev-shm-usage")
+#     opts.add_argument("--window-size=1400,800") # your desired window size
+#     # opts.add_argument("--no-sandbox")
+#     # pin browser version to 127
+#     # opts.add_argument("--browser-version=127")
 
-    opts.browser_version = "127"
-  end
+#     opts.browser_version = "127"
+#   end
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+# end
 
 class ActionDispatch::IntegrationTest
   include Rails.application.routes.url_helpers
