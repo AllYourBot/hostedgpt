27e9378b56ede8f2a359c8a44500a5e8bc87b514
Justin Vallelonga <jlvallelonga@gmail.com>
2024-10-15 11:56:22 +0700

    upgrades solid_queue to 0.6.x

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_15_044832) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_15_045443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -328,7 +328,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_15_044832) do
     t.string "hostname"
     t.text "metadata"
     t.datetime "created_at", null: false
+    t.string "name", null: false
     t.index ["last_heartbeat_at"], name: "index_solid_queue_processes_on_last_heartbeat_at"
+    t.index ["name", "supervisor_id"], name: "index_solid_queue_processes_on_name_and_supervisor_id", unique: true
     t.index ["supervisor_id"], name: "index_solid_queue_processes_on_supervisor_id"
   end
 
@@ -359,7 +361,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_15_044832) do
     t.text "arguments"
     t.string "queue_name"
     t.integer "priority", default: 0
-    t.boolean "static", default: true
+    t.boolean "static", default: true, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
