b89d608625c6cb53764bd660fb8d8a4b225ed0ab
Justin Vallelonga <jlvallelonga@gmail.com>
2024-10-21 17:36:15 +0700

    upgrade rails to 7.2

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source "https://rubygems.org"
 
 ruby file: ".ruby-version"
 
-gem "rails", "~> 7.1.4"
+gem "rails", "~> 7.2.0"
 gem "sprockets-rails" # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "pg", "~> 1.1"
 gem "puma", ">= 5.0"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  # Skip http-to-https redirect for the default health check endpoint.
+  # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
+
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new($stdout)
     .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
@@ -71,6 +74,8 @@ Rails.application.configure do
 
   # config.active_job.queue_name_prefix = "hostedgpt_production"
 
+  # Disable caching for Action Mailer templates even if Action Controller
+  # caching is enabled.
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
