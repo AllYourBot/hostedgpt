4ff1b4f966da11584a1c459ae0ae2d3bf6d63e63
Justin Vallelonga <jlvallelonga@gmail.com>
2024-10-27 14:08:04 +0700

    adds replacement method for message model

--- config/initializers/extend_active_record.rb
+++ config/initializers/extend_active_record.rb
@@ -10,13 +10,20 @@ module ExtendActiveRecord
     end
   end
 
-  def count(*args)
-    if to_sql.include?("DISTINCT ON")
-      unscoped.from("(#{reorder(nil).to_sql}) AS subq").select("count(*)").to_a.first["count"].to_i
-    else
-      super(*args)
-    end
-  end
+  # def count(*args)
+  #   if to_sql.include?("DISTINCT ON")
+  #     # print stack trace
+  #     puts "stack trace in count:"
+  #     puts caller.join("\n")
+  #     before_sql = to_sql
+  #     puts "before_sql: #{before_sql}"
+  #     unscoped_sql = unscoped.from("(#{reorder(nil).to_sql}) AS subq").select("count(*)").to_sql
+  #     puts "unsoped_sql: #{unscoped_sql}"
+  #     unscoped.from("(#{reorder(nil).to_sql}) AS subq").select("count(*)").to_a.first["count"].to_i
+  #   else
+  #     super(*args)
+  #   end
+  # end
 end
 
 ActiveRecord::Relation.prepend(ExtendActiveRecord)
