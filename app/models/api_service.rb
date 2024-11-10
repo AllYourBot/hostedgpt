8c8fd5238481b201d8d492ea06e43fc6d0237957
Keith Schacht <krschacht@gmail.com>
2024-11-09 19:44:48 -0600

    Remove whitespace from the merge conflict resolution

--- app/models/api_service.rb
+++ app/models/api_service.rb
@@ -7,7 +7,7 @@ class APIService < ApplicationRecord
   belongs_to :user
 
   has_many :language_models, -> { not_deleted }
-  
+
   enum :driver, %w[ openai anthropic gemini].index_by(&:to_sym)
 
   validates :url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https]), if: -> { url.present? }
