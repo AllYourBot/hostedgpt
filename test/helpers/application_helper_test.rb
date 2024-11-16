b7d8dc184a5a379e3848d67552a7b23a257dd394
Paul Vudmaska <paul@vudmaska.com>
2024-11-15 19:56:36 -0600

    User can only have 2 initials in UI (#544)
    Co-authored-by: Keith Schacht <krschacht@gmail.com>

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+class ApplicationHelperTest < ActionView::TestCase
+
+  test "only at most 2 initials" do
+    assert_equal "jz", at_most_two_initials("jdz")
+  end
+
+  test "single initials allowed" do
+    assert_equal "q", at_most_two_initials("q")
+  end
+
+  test "nil returns nil" do
+    assert_nil at_most_two_initials(nil)
+  end
+
+  test "can have numbers" do
+    assert_equal "P2", at_most_two_initials("P2")
+  end
+
+  test "does not change case" do
+    assert_equal "p2", at_most_two_initials("p2")
+  end
+
+  test "returns the correct two initials when there are more than two" do
+    assert_equal "kS", at_most_two_initials("kRxS")
+  end
+
+  test "can have spaces" do
+    assert_equal "pQ", at_most_two_initials("p v Q")
+  end
+end
