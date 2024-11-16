15d51b6198ba3649d17c80b36ae3b609cc6e5510
Keith Schacht <krschacht@gmail.com>
2024-11-15 19:44:00 -0600

    Clean up whitespace

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,31 +1,28 @@
 require "test_helper"
 
 class ApplicationHelperTest < ActionView::TestCase
-  
-test "only at most 2 initials" do
+
+  test "only at most 2 initials" do
     assert_equal "jz", at_most_two_initials("jdz")
   end
-  
 
   test "single initials allowed" do
     assert_equal "q", at_most_two_initials("q")
-  end  
-
+  end
 
   test "nil returns nil" do
     assert_nil at_most_two_initials(nil)
-  end   
+  end
 
   test "can have numbers" do
     assert_equal "P2", at_most_two_initials("P2")
-  end  
+  end
 
   test "does not change case" do
     assert_equal "p2", at_most_two_initials("p2")
-  end  
+  end
 
   test "can have spaces" do
     assert_equal "pQ", at_most_two_initials("p v Q")
-  end  
-
+  end
 end 
