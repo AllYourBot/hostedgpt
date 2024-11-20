1bbbbe45c99758b4d41381d723df4e8eb44fdc32
Justin Vallelonga <jlvallelonga@gmail.com>
2024-11-20 19:37:16 +0700

    modifies init script to check for user before creating

--- docker/01-postgres-init-rails-user.sh
+++ docker/01-postgres-init-rails-user.sh
@@ -4,7 +4,7 @@ set -e
 # the script uses the DATABASE_URL env var (if set)
 # to extract the username and password and set HOSTEDGPT_DB_USERNAME and HOSTEDGPT_DB_PASSWORD
 # if DATABASE_URL is not set, it uses the existing HOSTEDGPT_DB_USERNAME and HOSTEDGPT_DB_PASSWORD
-# to create the rails user
+# to create the rails user if it doesn't already exist
 
 if [ -n "$DATABASE_URL" ]; then
   echo "DATABASE_URL is set. Extracting username and password"
@@ -14,5 +14,17 @@ fi
 
 echo "creating user $HOSTEDGPT_DB_USERNAME"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOS
-  CREATE USER $HOSTEDGPT_DB_USERNAME WITH SUPERUSER PASSWORD '$HOSTEDGPT_DB_PASSWORD';
+  DO
+  \$do\$
+  BEGIN
+    IF EXISTS (
+        SELECT FROM pg_catalog.pg_roles
+        WHERE  rolname = '$HOSTEDGPT_DB_USERNAME') THEN
+
+        RAISE NOTICE 'Role "$HOSTEDGPT_DB_USERNAME" already exists. Skipping.';
+    ELSE
+        CREATE USER $HOSTEDGPT_DB_USERNAME WITH SUPERUSER PASSWORD '$HOSTEDGPT_DB_PASSWORD';
+    END IF;
+  END
+  \$do\$;
 EOS
