d412278ce7f2f6ac75cbf4a703a698e5f74c6455
Keith Schacht <krschacht@gmail.com>
2024-11-20 21:34:55 -0600

    white space

--- app/models/api_service.rb
+++ app/models/api_service.rb
@@ -8,7 +8,7 @@ class APIService < ApplicationRecord
 
   has_many :language_models, -> { not_deleted }
 
-  enum :driver, %w[ openai anthropic gemini].index_by(&:to_sym)
+  enum :driver, %w[openai anthropic gemini].index_by(&:to_sym)
 
   validates :url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https]), if: -> { url.present? }
   validates :name, :url, presence: true
@@ -22,17 +22,17 @@ class APIService < ApplicationRecord
 
   def ai_backend
     case driver
-      when "openai"
-        AIBackend::OpenAI
-      when "anthropic"
-        AIBackend::Anthropic
-      when "gemini"
-        AIBackend::Gemini
+    when "openai"
+      AIBackend::OpenAI
+    when "anthropic"
+      AIBackend::Anthropic
+    when "gemini"
+      AIBackend::Gemini
     end
   end
 
   def requires_token?
-    [URL_OPEN_AI, URL_ANTHROPIC,URL_GEMINI].include?(url) # other services may require it but we don't always know
+    [URL_OPEN_AI, URL_ANTHROPIC, URL_GEMINI].include?(url) # other services may require it but we don't always know
   end
 
   def effective_token
