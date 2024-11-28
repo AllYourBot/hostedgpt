dfd329282c162aa1f959b59326b2a065967d621e
Keith Schacht <krschacht@gmail.com>
2024-11-27 19:45:20 -0600

    Reword tests in messages_helper_test.rb

--- test/helpers/messages_helper_test.rb
+++ test/helpers/messages_helper_test.rb
@@ -2,17 +2,17 @@ require "test_helper"
 
 class MessagesHelperTest < ActionView::TestCase
 
-  test "recognizes memory updated" do
+  test "memory_updated responds properly for tool result" do
     message = messages(:keep_memory_tool_result)
     assert memory_updated?(message)
   end
 
-  test "recognizes message to user from tool call" do
+  test "message_to_user_from_tool_call responds properly for tool result" do
     message = messages(:keep_memory_tool_result)
     assert message_to_user_from_tool_call?(message)
   end
 
-  test "recognizes message to user from tool call" do
+  test "from_name_for responds properly for tool result" do
     message = messages(:keep_memory_tool_result)
     assert_nil from_name_for(message)
   end
