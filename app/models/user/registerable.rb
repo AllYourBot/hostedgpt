a8cad5e048c9e5ab43582a478310265333a8405f
Dr Nic Williams <drnicwilliams@gmail.com>
2024-11-28 13:08:02 +1000

    Slugs without dots

--- app/models/user/registerable.rb
+++ app/models/user/registerable.rb
@@ -11,6 +11,7 @@ module User::Registerable
     api_services.create!(url: APIService::URL_OPEN_AI, driver: :openai, name: "OpenAI")
     api_services.create!(url: APIService::URL_ANTHROPIC, driver: :anthropic, name: "Anthropic")
     api_services.create!(url: APIService::URL_GROQ, driver: :openai, name: "Groq")
+    api_services.create!(url: APIService::URL_GEMINI, driver: :gemini, name: "Google Gemini")
 
     LanguageModel.import_from_file(users: [self])
     Assistant.import_from_file(users: [self])
--- db/migrate/20241126084628_add_gemini.rb
+++ db/migrate/20241126084628_add_gemini.rb
@@ -1,24 +1,11 @@
 class AddGemini < ActiveRecord::Migration[7.2]
   def up
     User.all.find_each do |user|
-      gemini_api_service = user.api_services.create!(url: APIService::URL_GEMINI, driver: :gemini, name: "Google Gemini")
-
-      language_model = user.language_models.create!(
-        api_name: "gemini-1.5-pro-002",
-        name: "Gemini Pro 1.5",
-        api_service: gemini_api_service,
-        supports_images: true
-      )
-
-      user.assistants.create!(
-        name: "Gemini Pro 1.5",
-        slug: "gemini-1.5-pro-002",
-        language_model: language_model
-      )
+      user.api_services.create!(url: APIService::URL_GEMINI, driver: :gemini, name: "Google Gemini")
     end
   end
 
   def down
-    raise "This migration can't be reversed easily."
+    APIService.where(url: APIService::URL_GEMINI, driver: :gemini).destroy_all
   end
 end
