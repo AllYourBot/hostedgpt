5d92c1c1af68df93861e10141d0120666a6530c3
Justin Vallelonga <jlvallelonga@gmail.com>
2024-11-30 20:32:51 +0700

    fixes merge issue

--- app/models/document.rb
+++ app/models/document.rb
@@ -55,18 +55,7 @@ class Document < ApplicationRecord
     )
   end
 
-  def file_base64(variant = :large)
-    return nil if !file.attached?
-    wait_for_file_variant_to_process!(variant.to_sym)
-    file_contents = file.variant(variant.to_sym).processed.download
-    base64 = Base64.strict_encode64(file_contents)
-  end
-
   private
-
-  def file_data_url(variant = :large)
-    "data:#{file.blob.content_type};base64,#{file_base64(variant)}"
-  end
 
   def set_default_user
     self.user ||= message.conversation.user
