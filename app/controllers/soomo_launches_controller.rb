fd162d74132b966a12d213979b8e90dce3e64232
Matthew Bennink <matthew.bennink@soomolearning.com>
2024-12-03 04:22:40 -0500

    Adds Claude 3.5 Sonnet v20241022

--- app/controllers/soomo_launches_controller.rb
+++ app/controllers/soomo_launches_controller.rb
@@ -61,7 +61,8 @@ class SoomoLaunchesController < ApplicationController
       ["GPT-4o mini", "gpt-4o-mini"],
       ["GPT-3.5", "gpt-3.5-turbo"],
       ["Claude 3 Opus", "claude-3-opus-20240229"],
-      ["Claude 3.5 Sonnet", "claude-3-5-sonnet-20240620"]
+      ["Claude 3.5 Sonnet", "claude-3-5-sonnet-20240620"],
+      ["Claude 3.5 Sonnet", "claude-3-5-sonnet-20241022"]
     ].map do |(assistant_name, model_name)|
       person.user.assistants.create!(
         name: assistant_name,
