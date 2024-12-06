4153945643a40b016430a437e5875903f4c08531
Matt Lindsey <matthew.r.lindsey@gmail.com>
2024-12-05 23:09:34 -0500

    Fix focus on blank query

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -28,12 +28,12 @@ class Conversation < ApplicationRecord
   #  "Older" => relation
   # }
   def self.grouped_by_increasing_time_interval_for_user(user, query = nil)
-    if query.present?
+    if query.blank?
+      nav_conversations = user.conversations.ordered
+    else
       nav_conversations = user.conversations.joins(:messages).ordered.where("messages.content_text ILIKE ?", "%#{query}%").
         or(user.conversations.ordered.where("title ILIKE ?", "%#{query}%")).
         select("DISTINCT conversations.*")
-    else
-      nav_conversations = user.conversations.ordered
     end
 
     keys = ["Today", "Yesterday", "This Week", "This Month", "Last Month", "Older"]
