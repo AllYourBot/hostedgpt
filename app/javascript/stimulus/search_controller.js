8614591ffb2cb7221fa33a7258ef69d1852d27b7
Matt Lindsey <matthew.r.lindsey@gmail.com>
2024-12-07 00:24:33 -0500

    Refactor js and restore focus fix

--- app/javascript/stimulus/search_controller.js
+++ app/javascript/stimulus/search_controller.js
@@ -3,27 +3,21 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "input" ]
 
-  connect() {
-    this.originalText = this.inputTarget.value
-    this.inputTarget.focus()
-    this.cursorToEnd()
-  }
-
   cursorToEnd() {
     this.inputTarget.selectionStart =
       this.inputTarget.selectionEnd =
         this.inputTarget.value.length
   }
 
-  submitForm() {
-    if (this.inputTarget.disabled) return
-
+  clear() {
+    this.inputTarget.value = ""
     this.element.requestSubmit()
-    this.inputTarget.disabled = true
   }
 
-  abort() {
-    this.inputTarget.value = this.originalText
-    this.submitForm()
+  search() {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      this.element.requestSubmit()
+    }, 900)
   }
 }
