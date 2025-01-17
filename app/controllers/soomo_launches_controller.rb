0c0788b87f09101914d27d59352d503bd6a02364
Matthew Bennink <matthew.bennink@soomolearning.com>
2025-01-17 08:26:47 -0500

    adds o1-mini

--- app/controllers/soomo_launches_controller.rb
+++ app/controllers/soomo_launches_controller.rb
@@ -58,6 +58,7 @@ class SoomoLaunchesController < ApplicationController
 
     assistants = [
       ["o1", "o1"],
+      ["o1-mini", "o1-mini"],
       ["GPT-4o", "gpt-4o"],
       ["GPT-4o mini", "gpt-4o-mini"],
       ["GPT-3.5", "gpt-3.5-turbo"],
