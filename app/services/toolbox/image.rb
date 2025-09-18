c605e571476eafd9039b7f5d69886183acff7a7c
Matt Lindsey <matthew.r.lindsey@gmail.com>
2025-09-18 17:05:45 -0400

    Fix tool description

--- app/services/toolbox/image.rb
+++ app/services/toolbox/image.rb
@@ -1,7 +1,7 @@
 class Toolbox::Image < Toolbox
 
   describe :generate_an_image, <<~S
-    Generate an image based on what the user asks you to generate. You will pass the user's prompt and will get back a URL to an image.
+    Generate an image based on what the user asks you to generate. You will pass the user's prompt and will get back the image.
   S
 
   def generate_an_image(image_generation_prompt_s:)
