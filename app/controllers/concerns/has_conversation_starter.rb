d6c2a0a2356db2268a2860a99c245883465f43ea
Matt Lindsey <matthew.r.lindsey@gmail.com>
2026-07-09 16:43:25 -0400

    Add one image starter

--- app/controllers/concerns/has_conversation_starter.rb
+++ app/controllers/concerns/has_conversation_starter.rb
@@ -5,8 +5,11 @@ module HasConversationStarter
 
   def set_conversation_starters
     starters = I18n.t("app.conversations.starters")
+    image_starters = I18n.t("app.conversations.image_starters")
   # Ensure we have an array of hashes with title/description
     starters = Array(starters).map { |s| [s[:title] || s["title"], s[:description] || s["description"]] }.select { |a| a[0].present? && a[1].present? }
-    @conversation_starters = starters.shuffle.take(4)
+    image_starters = Array(image_starters).map { |s| [s[:title] || s["title"], s[:description] || s["description"]] }.select { |a| a[0].present? && a[1].present? }
+
+    @conversation_starters = starters.shuffle.take(3) + image_starters.shuffle.take(1)
   end
 end
