667c8b7981f533ceeb0d13747fadf25bfecdaee5
Matt Lindsey <matthew.r.lindsey@gmail.com>
2026-08-15 15:45:16 -0400

    Fix home page for mobile devices (#764)

--- app/controllers/assistants_controller.rb
+++ app/controllers/assistants_controller.rb
@@ -3,7 +3,7 @@ class AssistantsController < ApplicationController
   before_action :set_nav_assistants
 
   def index
-    unless Feature.assistants_page?
+    unless Feature.assistants_page? && !mobile_device?
       best_assistant = @nav_assistants.first
       redirect_to new_assistant_message_path(best_assistant), status: :see_other
       return
--- test/controllers/assistants_controller_test.rb
+++ test/controllers/assistants_controller_test.rb
@@ -20,4 +20,11 @@ class AssistantsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_response :success
   end
+
+  test "index redirects to conversation on a mobile device even if assistants_page is enabled" do
+    stub_features(assistants_page: true) do
+      get assistants_url, headers: { "User-Agent" => "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1" }
+    end
+    assert_redirected_to new_assistant_message_path(@assistant)
+  end
 end
