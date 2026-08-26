0630fcb880cfe6c9ac0dc56282525ab5445b1121
Sandip Trivedi <trivedi.sandip@gmail.com>
2026-08-26 07:49:41 -0400

    Use activerecord store for the json column (#779)

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,9 +38,31 @@ class User < ApplicationRecord
 
   accepts_nested_attributes_for :credentials
   serialize :preferences, coder: JsonSerializer
+  store_accessor :preferences, :dark_mode, :nav_closed
 
   def preferences
-    attributes["preferences"].with_defaults(dark_mode: "system")
+    attributes["preferences"] || {}
+  end
+
+  def dark_mode
+    super.presence || "system"
+  end
+
+  def nav_closed=(value)
+    super(value.nil? ? nil : value.to_b)
+  end
+
+  def features
+    User::Features.new(self)
+  end
+
+  def ai_backend(driver)
+    features[:"#{driver}_ai_backend"]
+  end
+
+  # @todo: generalize into a per-flag opinion API if other features ever need per-user values (#740)
+  def ruby_llm?(driver)
+    ai_backend(driver).presence || use_ruby_llm?
   end
 
   # Profile picture helper methods
@@ -92,5 +114,9 @@ class User < ApplicationRecord
     return false unless credential = credentials.first
 
     !credential.persisted? && credential.type == "GoogleCredential"
+  end
+
+  def use_ruby_llm?
+    Feature.raw_features.key?(:use_ruby_llm) ? Feature.use_ruby_llm? : false
   end
 end
---
+++ app/models/user/features.rb
@@ -0,0 +1,47 @@
+class User::Features
+  def self.derived_backend_names
+    APIService.drivers.keys
+      .select { |driver| APIService.new(driver: driver).ai_backend }
+      .map { |driver| :"#{driver}_ai_backend" }
+  end
+
+  def self.valid_names
+    derived_backend_names + [:use_ruby_llm]
+  end
+
+  def self.ruby_llm_available?(driver)
+    defined?(AIBackend::RubyLLM) &&
+      AIBackend::RubyLLM.respond_to?(:supports_driver?) &&
+      AIBackend::RubyLLM.supports_driver?(driver)
+  end
+
+  def initialize(user)
+    @user = user
+  end
+
+  def [](name)
+    key = guard_name!(name)
+    feature_preferences[key]
+  end
+
+  def []=(name, value)
+    key = guard_name!(name)
+    @user.update!(preferences: @user.preferences.deep_merge(feature: { key => value }))
+  end
+
+  private
+
+  def feature_preferences
+    sub_hash = @user.preferences[:feature] || @user.preferences["feature"] || {}
+    sub_hash.transform_keys(&:to_sym)
+  end
+
+  def guard_name!(name)
+    key = name.to_s.chomp("=").to_sym
+    unless self.class.valid_names.include?(key)
+      raise KeyError, "You attempted to reference '#{key}' but only AI-backend choices (<driver>_ai_backend) and use_ruby_llm are accessible here. Did you typo a feature name?"
+    end
+
+    key
+  end
+end
